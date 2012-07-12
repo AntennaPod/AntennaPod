5b57490f60582d38c8e95c28860cdd0db32ae8a4
daniel oeh <daniel.oeh@gmail.com>
2012-07-12 16:14:38 +0200

    Added item visibility check and 'revoke access'-item

--- src/de/podfetcher/activity/PreferenceActivity.java
+++ src/de/podfetcher/activity/PreferenceActivity.java
@@ -10,25 +10,44 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 import de.podfetcher.R;
+import de.podfetcher.util.FlattrUtils;
 
 public class PreferenceActivity extends SherlockPreferenceActivity {
 	private static final String TAG = "PreferenceActivity";
-	
+
 	private static final String PREF_FLATTR_THIS_APP = "prefFlattrThisApp";
-	
+	private static final String PREF_FLATTR_AUTH = "pref_flattr_authenticate";
+	private static final String PREF_FLATTR_REVOKE = "prefRevokeAccess";
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		addPreferencesFromResource(R.xml.preferences);
-		findPreference(PREF_FLATTR_THIS_APP).setOnPreferenceClickListener(new OnPreferenceClickListener() {
+		findPreference(PREF_FLATTR_THIS_APP).setOnPreferenceClickListener(
+				new OnPreferenceClickListener() {
 
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				Log.d(TAG, "Flattring this app");
-				return true;
-			}});
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						Log.d(TAG, "Flattring this app");
+						return true;
+					}
+				});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		checkItemVisibility();
+	}
+
+	@SuppressWarnings("deprecation")
+	private void checkItemVisibility() {
+		boolean hasFlattrToken = FlattrUtils.hasToken();
+		findPreference(PREF_FLATTR_AUTH).setEnabled(!hasFlattrToken);
+		findPreference(PREF_FLATTR_REVOKE).setEnabled(hasFlattrToken);
+
 	}
 
 	@Override
