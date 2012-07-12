f3c19b6da20dadc3b93b6e1d2fd8481f9afb561b
daniel oeh <daniel.oeh@gmail.com>
2012-07-12 16:06:10 +0200

    Added preference item to flattr this app

--- src/de/podfetcher/activity/PreferenceActivity.java
+++ src/de/podfetcher/activity/PreferenceActivity.java
@@ -1,6 +1,9 @@
 package de.podfetcher.activity;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.Menu;
@@ -9,11 +12,23 @@ import com.actionbarsherlock.view.MenuItem;
 import de.podfetcher.R;
 
 public class PreferenceActivity extends SherlockPreferenceActivity {
+	private static final String TAG = "PreferenceActivity";
+	
+	private static final String PREF_FLATTR_THIS_APP = "prefFlattrThisApp";
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		addPreferencesFromResource(R.xml.preferences);
+		findPreference(PREF_FLATTR_THIS_APP).setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Log.d(TAG, "Flattring this app");
+				return true;
+			}});
 	}
 
 	@Override
