fb4ba95789a7d59379904a7ec3d6919eff7c7cc9
daniel oeh <daniel.oeh@gmail.com>
2012-10-27 22:16:07 +0200

    Removed unused constructor

--- src/de/danoeh/antennapod/feed/FeedItem.java
+++ src/de/danoeh/antennapod/feed/FeedItem.java
@@ -41,18 +41,6 @@ public class FeedItem extends FeedComponent {
 		this.read = true;
 	}
 
-	public FeedItem(String title, String description, String link,
-			Date pubDate, FeedMedia media, Feed feed) {
-		super();
-		this.title = title;
-		this.description = description;
-		this.link = link;
-		this.pubDate = pubDate;
-		this.media = media;
-		this.feed = feed;
-		this.read = true;
-	}
-
 	/** Get the chapter that fits the position. */
 	public Chapter getCurrentChapter(int position) {
 		Chapter current = null;
