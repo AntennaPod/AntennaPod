50072c4ebbc60006c04974650a75c243a31cc35c
daniel oeh <daniel.oeh@gmail.com>
2012-11-23 20:13:26 +0100

    PreferenceActivity now applies its theme correctly

--- src/de/danoeh/antennapod/activity/PreferenceActivity.java
+++ src/de/danoeh/antennapod/activity/PreferenceActivity.java
@@ -1,6 +1,7 @@
 package de.danoeh.antennapod.activity;
 
 import android.content.Intent;
+import android.content.res.Resources.Theme;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -131,5 +132,12 @@ public class PreferenceActivity extends SherlockPreferenceActivity {
 		}
 		return true;
 	}
+
+	@Override
+	protected void onApplyThemeResource(Theme theme, int resid, boolean first) {
+		theme.applyStyle(PodcastApp.getThemeResourceId(), true);
+	}
+	
+	
 
 }
