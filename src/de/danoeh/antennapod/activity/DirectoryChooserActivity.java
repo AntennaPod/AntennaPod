8726ea18c07e57d9cb3fd94acb5ee0c34149d305
daniel oeh <daniel.oeh@gmail.com>
2012-12-22 23:55:35 +0100

    Created layout for directorychooser

--- src/de/danoeh/antennapod/activity/DirectoryChooserActivity.java
+++ src/de/danoeh/antennapod/activity/DirectoryChooserActivity.java
@@ -1,16 +1,47 @@
 package de.danoeh.antennapod.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+
+import de.danoeh.antennapod.PodcastApp;
+import de.danoeh.antennapod.R;
 
 /** Let's the user choose a directory on the storage device. */
-public abstract class DirectoryChooserActivity extends SherlockActivity {
+public class DirectoryChooserActivity extends SherlockActivity {
 	private static final String TAG = "DirectoryChooserActivity";
 
+	private Button butConfirm;
+	private Button butCancel;
+	private Button butNavUp;
+	private TextView txtvSelectedFolder;
+	private ListView listDirectories;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(PodcastApp.getThemeResourceId());
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.directory_chooser);
+		butConfirm = (Button) findViewById(R.id.butConfirm);
+		butCancel = (Button) findViewById(R.id.butCancel);
+		butNavUp = (Button) findViewById(R.id.butNavUp);
+		txtvSelectedFolder = (TextView) findViewById(R.id.txtvSelectedFolder);
+		listDirectories = (ListView) findViewById(R.id.directory_list);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = new MenuInflater(this);
+		inflater.inflate(R.menu.directory_chooser, menu);
+		return true;
+	}
+	
+	
 	
 }
