f7304266446dc6b4347806274bd955f7ed62977e
daniel oeh <daniel.oeh@gmail.com>
2013-02-25 22:35:12 +0100

    License.html wasn't loaded correctly

--- src/de/danoeh/antennapod/activity/AboutActivity.java
+++ src/de/danoeh/antennapod/activity/AboutActivity.java
@@ -2,6 +2,7 @@ package de.danoeh.antennapod.activity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
@@ -18,6 +19,15 @@ public class AboutActivity extends SherlockActivity {
 		getSupportActionBar().hide();
 		setContentView(R.layout.about);		
 		webview = (WebView) findViewById(R.id.webvAbout);
+		webview.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				view.loadUrl(url);
+				return false;
+			}
+			
+		});
 		webview.loadUrl("file:///android_asset/about.html");
 	}
 
