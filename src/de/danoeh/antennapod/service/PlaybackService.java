3f8e6c26a348562799762ae1289195e557a02eaa
Tom Hennen <tom.hennen@gmail.com>
2013-08-14 12:26:03 -0400

    Adds an option to pause audio while audio focus is lost.  This is helpful when using navigation apps (for example) so that you can hear what the navigation app says and you don't miss whatever was being said on your podcast. Without this setting checked they may wind up talking over each other, which can be confusing.

--- src/de/danoeh/antennapod/service/PlaybackService.java
+++ src/de/danoeh/antennapod/service/PlaybackService.java
@@ -353,11 +353,18 @@ public class PlaybackService extends Service {
                     break;
                 case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
                     if (status == PlayerStatus.PLAYING) {
-                        if (AppConfig.DEBUG)
-                            Log.d(TAG, "Lost audio focus temporarily. Ducking...");
-                        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
-                                AudioManager.ADJUST_LOWER, 0);
-                        pausedBecauseOfTransientAudiofocusLoss = true;
+                        if (!UserPreferences.shouldPauseForNotifications()) {
+                            if (AppConfig.DEBUG)
+                                Log.d(TAG, "Lost audio focus temporarily. Ducking...");
+                            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                                    AudioManager.ADJUST_LOWER, 0);
+                            pausedBecauseOfTransientAudiofocusLoss = true;
+                        } else {
+                            if (AppConfig.DEBUG)
+                                Log.d(TAG, "Lost audio focus temporarily. Could duck, but won't, pausing...");
+                            pause(false, false);
+                            pausedBecauseOfTransientAudiofocusLoss = true;
+                        }
                     }
                     break;
                 case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
