e8d0abe954b6cda97c99f90e9b0da75e8fa5f73a
daniel oeh <daniel.oeh@gmail.com>
2013-08-23 14:58:09 +0200

    Catch RuntimeException thrown by MediaPlayer constructor

--- src/de/danoeh/antennapod/service/download/DownloadService.java
+++ src/de/danoeh/antennapod/service/download/DownloadService.java
@@ -828,8 +828,9 @@ public class DownloadService extends Service {
             media.setFile_url(request.getDestination());
 
             // Get duration
-            MediaPlayer mediaplayer = new MediaPlayer();
+            MediaPlayer mediaplayer = null;
             try {
+                mediaplayer = new MediaPlayer();
                 mediaplayer.setDataSource(media.getFile_url());
                 mediaplayer.prepare();
                 media.setDuration(mediaplayer.getDuration());
@@ -838,8 +839,13 @@ public class DownloadService extends Service {
                 mediaplayer.reset();
             } catch (IOException e) {
                 e.printStackTrace();
+            } catch (RuntimeException e) {
+                // Thrown by MediaPlayer initialization on some devices
+                e.printStackTrace();
             } finally {
-                mediaplayer.release();
+                if (mediaplayer != null) {
+                    mediaplayer.release();
+                }
             }
 
             if (media.getItem().getChapters() == null) {
