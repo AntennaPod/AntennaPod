730ba3cc26d65a2f4c6c8f2489ea98beab10364b
daniel oeh <daniel.oeh@gmail.com>
2013-09-01 16:27:01 +0200

    Added suggestions list
    Turned off at the moment

--- src/de/danoeh/antennapod/activity/gpoddernet/GpodnetMainActivity.java
+++ src/de/danoeh/antennapod/activity/gpoddernet/GpodnetMainActivity.java
@@ -7,7 +7,9 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.fragment.gpodnet.PodcastTopListFragment;
+import de.danoeh.antennapod.fragment.gpodnet.SuggestionListFragment;
 import de.danoeh.antennapod.fragment.gpodnet.TagListFragment;
+import de.danoeh.antennapod.preferences.GpodnetPreferences;
 
 /**
  * Created by daniel on 22.08.13.
@@ -17,6 +19,7 @@ public class GpodnetMainActivity extends GpodnetActivity {
 
     private static final int POS_TAGS = 0;
     private static final int POS_TOPLIST = 1;
+    private static final int POS_SUGGESTIONS = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +32,13 @@ public class GpodnetMainActivity extends GpodnetActivity {
 
     private class PagerAdapter extends FragmentStatePagerAdapter {
 
-        private static final int NUM_PAGES = 2;
+        private static final int NUM_PAGES_LOGGED_OUT = 2;
+        private static final int NUM_PAGES_LOGGED_IN = 3;
+        private final int NUM_PAGES;
 
         public PagerAdapter(FragmentManager fm) {
             super(fm);
+            NUM_PAGES = NUM_PAGES_LOGGED_OUT;
         }
 
         @Override
@@ -42,6 +48,8 @@ public class GpodnetMainActivity extends GpodnetActivity {
                     return new TagListFragment();
                 case POS_TOPLIST:
                     return new PodcastTopListFragment();
+                case POS_SUGGESTIONS:
+                    return new SuggestionListFragment();
                 default:
                     return null;
             }
@@ -54,6 +62,8 @@ public class GpodnetMainActivity extends GpodnetActivity {
                     return getString(R.string.gpodnet_taglist_header);
                 case POS_TOPLIST:
                     return getString(R.string.gpodnet_toplist_header);
+                case POS_SUGGESTIONS:
+                    return getString(R.string.gpodnet_suggestions_header);
                 default:
                     return super.getPageTitle(position);
             }
