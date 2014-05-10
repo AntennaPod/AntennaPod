cd845c3d6e19a9895174d1f1e22c249fb691cfb7
daniel oeh <daniel.oeh@gmail.com>
2014-05-11 00:52:55 +0200

    Fixed: gpodder.net taglist searched local subscriptions instead of gpodder.net

--- src/de/danoeh/antennapod/fragment/gpodnet/TagListFragment.java
+++ src/de/danoeh/antennapod/fragment/gpodnet/TagListFragment.java
@@ -37,12 +37,12 @@ public class TagListFragment extends ListFragment {
         super.onCreateOptionsMenu(menu, inflater);
         final SearchView sv = new SearchView(getActivity());
         MenuItemUtils.addSearchItem(menu, sv);
-        sv.setQueryHint(getString(R.string.search_hint));
+        sv.setQueryHint(getString(R.string.gpodnet_search_hint));
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 sv.clearFocus();
-                ((MainActivity) getActivity()).loadChildFragment(SearchFragment.newInstance(s));
+                ((MainActivity) getActivity()).loadChildFragment(SearchListFragment.newInstance(s));
                 return true;
             }
 
