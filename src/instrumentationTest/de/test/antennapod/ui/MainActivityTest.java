662f8a46794f33276582783ccc2f24486651eb18
daniel oeh <daniel.oeh@gmail.com>
2014-06-01 13:13:17 +0200

    Finished UITestUtils implementation

--- src/instrumentationTest/de/test/antennapod/ui/MainActivityTest.java
+++ src/instrumentationTest/de/test/antennapod/ui/MainActivityTest.java
@@ -10,14 +10,24 @@ import de.danoeh.antennapod.activity.MainActivity;
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private Solo solo;
+    private UITestUtils uiTestUtils;
 
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
+    public MainActivityTest() {
+        super(MainActivity.class);
     }
 
     @Override
     protected void setUp() throws Exception {
+        super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
+        uiTestUtils = new UITestUtils(getInstrumentation().getTargetContext());
+        uiTestUtils.setup();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        uiTestUtils.tearDown();
+        super.tearDown();
     }
 
 
