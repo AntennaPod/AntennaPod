594dd8cab9fcbe1eea762f055cb2693ffa8dec46
daniel oeh <daniel.oeh@gmail.com>
2014-12-04 22:17:21 +0100

    Use toolbar in MainActivity instead of actionbar

--- app/src/main/java/de/danoeh/antennapod/activity/MainActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -66,6 +67,7 @@ public class MainActivity extends ActionBarActivity implements NavDrawerActivity
             POS_HISTORY = 3,
             POS_ADD = 4;
 
+    private Toolbar toolbar;
     private ExternalPlayerFragment externalPlayerFragment;
     private DrawerLayout drawerLayout;
 
@@ -81,14 +83,14 @@ public class MainActivity extends ActionBarActivity implements NavDrawerActivity
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        setTheme(UserPreferences.getTheme());
+        setTheme(UserPreferences.getNoTitleTheme());
         super.onCreate(savedInstanceState);
         StorageUtils.checkStorageAvailability(this);
         setContentView(R.layout.main);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         drawerTitle = currentTitle = getTitle();
-
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         navList = (ListView) findViewById(R.id.nav_list);
