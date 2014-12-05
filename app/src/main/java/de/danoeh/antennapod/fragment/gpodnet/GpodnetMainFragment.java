120db7ae7e7edf0716cbdebfac024f96a0888d40
daniel oeh <daniel.oeh@gmail.com>
2014-12-05 10:36:09 +0100

    Replaced tabs with viewpager indicators
    Tabs are not supported when using Toolbars

--- app/src/main/java/de/danoeh/antennapod/fragment/gpodnet/GpodnetMainFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/gpodnet/GpodnetMainFragment.java
@@ -1,83 +1,30 @@
 package de.danoeh.antennapod.fragment.gpodnet;
 
-import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.activity.MainActivity;
 
 /**
  * Main navigation hub for gpodder.net podcast directory
  */
 public class GpodnetMainFragment extends Fragment {
 
-    private ViewPager pager;
-    private MainActivity activity;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View root = inflater.inflate(R.layout.pager_fragment, container, false);
-        pager = (ViewPager) root.findViewById(R.id.pager);
+        ViewPager pager = (ViewPager) root.findViewById(R.id.pager);
         GpodnetPagerAdapter pagerAdapter = new GpodnetPagerAdapter(getChildFragmentManager(), getResources());
         pager.setAdapter(pagerAdapter);
-        final ActionBar actionBar = activity.getMainActivtyActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-            @Override
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-                pager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-            }
-
-            @Override
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-            }
-        };
-        actionBar.removeAllTabs();
-        actionBar.addTab(actionBar.newTab()
-                .setText(R.string.gpodnet_taglist_header)
-                .setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab()
-                .setText(R.string.gpodnet_toplist_header)
-                .setTabListener(tabListener));
-        actionBar.setTitle(R.string.gpodnet_main_label);
-
-        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
         return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        activity.getMainActivtyActionBar().removeAllTabs();
-        activity.getMainActivtyActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        this.activity = (MainActivity) activity;
     }
 
     public class GpodnetPagerAdapter extends FragmentPagerAdapter {
