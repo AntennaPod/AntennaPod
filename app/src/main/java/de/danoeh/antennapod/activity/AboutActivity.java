77647cc154b5acbbde3f7535e74edb2df0da8acf
daniel oeh <daniel.oeh@gmail.com>
2014-12-08 10:35:02 +0100

    Destroy webview when it is no longer used
    Not destroying the webview might have caused crashes on some devices

--- app/src/main/java/de/danoeh/antennapod/activity/AboutActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/AboutActivity.java
@@ -4,29 +4,43 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
 import de.danoeh.antennapod.R;
 
-/** Displays the 'about' screen */
+/**
+ * Displays the 'about' screen
+ */
 public class AboutActivity extends ActionBarActivity {
 
-	private WebView webview;
+    private WebView webview;
+    private LinearLayout webviewContainer;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getSupportActionBar().hide();
-		setContentView(R.layout.about);		
-		webview = (WebView) findViewById(R.id.webvAbout);
-		webview.setWebViewClient(new WebViewClient() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.about);
+        webviewContainer = (LinearLayout) findViewById(R.id.webvContainer);
+        webview = (WebView) findViewById(R.id.webvAbout);
+        webview.setWebViewClient(new WebViewClient() {
 
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				return false;
-			}
-			
-		});
-		webview.loadUrl("file:///android_asset/about.html");
-	}
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
 
+        });
+        webview.loadUrl("file:///android_asset/about.html");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webviewContainer != null && webview != null) {
+            webviewContainer.removeAllViews();
+            webview.destroy();
+        }
+    }
 }
