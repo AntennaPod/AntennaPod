1bf02167887247b543f6e821d02ac641ba6039b8
Martin Fietz <Martin.Fietz@gmail.com>
2015-05-11 23:29:52 +0200

    Fix move to top/bottom

--- app/src/main/java/de/danoeh/antennapod/menuhandler/FeedItemMenuHandler.java
+++ app/src/main/java/de/danoeh/antennapod/menuhandler/FeedItemMenuHandler.java
@@ -72,10 +72,10 @@ public class FeedItemMenuHandler {
         }
 
         boolean isInQueue = queueAccess.contains(selectedItem.getId());
-        if(queueAccess.size() == 0 || queueAccess.get(0) != selectedItem.getId()) {
+        if(queueAccess.size() == 0 || queueAccess.get(0) == selectedItem.getId()) {
             mi.setItemVisibility(R.id.move_to_top_item, false);
         }
-        if(queueAccess.size() == 0 || queueAccess.get(queueAccess.size()-1) != selectedItem.getId()) {
+        if(queueAccess.size() == 0 || queueAccess.get(queueAccess.size()-1) == selectedItem.getId()) {
             mi.setItemVisibility(R.id.move_to_bottom_item, false);
         }
         if (!isInQueue || isPlaying) {
