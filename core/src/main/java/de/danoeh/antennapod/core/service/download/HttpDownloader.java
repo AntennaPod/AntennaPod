dbf46dfcd8e35d52b6ca3f9ae534f6f3149443ac
Martin Fietz <Martin.Fietz@gmail.com>
2015-06-04 09:40:27 +0200

    Fix tests

--- core/src/main/java/de/danoeh/antennapod/core/service/download/HttpDownloader.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/download/HttpDownloader.java
@@ -214,7 +214,7 @@ public class HttpDownloader extends Downloader {
                                     request.getSize()
                     );
                     return;
-                } else if(request.getSoFar() == 0){
+                } else if(request.getSize() > 0 && request.getSoFar() == 0){
                     onFail(DownloadError.ERROR_IO_ERROR, "Download completed, but nothing was read");
                     return;
                 }
