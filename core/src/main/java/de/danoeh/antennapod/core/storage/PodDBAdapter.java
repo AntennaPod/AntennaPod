fe6796f102bb8c126cfad80d957bffe0cb04fa34
Tom Hennen <tom.hennen@gmail.com>
2015-08-04 21:25:25 -0400

    Updated version code for RC5
    Also updated the oldVersion used by the UPDATE FeedItems sql statement.
    We don't want to run the 1030002 version ever, and the 1030005 version
    should fix any users that used the 1030002 version (with the possibility
    that some items that shouldn't have had auto_download set to 0 did have
    it set to 0, but accepting that that's not a terrible problem).

--- core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
+++ core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
@@ -1709,7 +1709,7 @@ public class PodDBAdapter {
                 db.execSQL("ALTER TABLE " + PodDBAdapter.TABLE_NAME_FEEDS
                         + " ADD COLUMN " + PodDBAdapter.KEY_AUTO_DELETE_ACTION + " INTEGER DEFAULT 0");
             }
-            if(oldVersion < 1030002) {
+            if(oldVersion < 1030005) {
                 db.execSQL("UPDATE FeedItems SET auto_download=0 WHERE " +
                         "(read=1 OR id IN (SELECT feeditem FROM FeedMedia WHERE position>0 OR downloaded=1)) " +
                         "AND id NOT IN (SELECT feeditem FROM Queue)");
