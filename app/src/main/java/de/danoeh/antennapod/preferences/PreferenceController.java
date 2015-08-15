3148b8835b126b1afa5fc65653b07f9909b7582d
Martin Fietz <Martin.Fietz@gmail.com>
2015-08-15 21:16:51 +0200

    Make res.getQuantityString replace placeholder with value

--- app/src/main/java/de/danoeh/antennapod/preferences/PreferenceController.java
+++ app/src/main/java/de/danoeh/antennapod/preferences/PreferenceController.java
@@ -428,7 +428,7 @@ public class PreferenceController {
                 entries[x] = res.getString(R.string.pref_smart_mark_as_played_disabled);
             } else {
                 Integer v = Integer.parseInt(values[x]);
-                entries[x] = res.getQuantityString(R.plurals.time_seconds_quantified, v);
+                entries[x] = res.getQuantityString(R.plurals.time_seconds_quantified, v, v);
             }
         }
         pref.setEntries(entries);
