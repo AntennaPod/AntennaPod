ef4f40757a7f72a7d45a1f5ac63452159510d92c
Tom Hennen <tom.hennen@gmail.com>
2015-11-28 17:25:33 -0500

    start app if playback not running

--- app/src/main/java/de/danoeh/antennapod/receiver/PlayerWidget.java
+++ app/src/main/java/de/danoeh/antennapod/receiver/PlayerWidget.java
@@ -13,37 +13,41 @@ import de.danoeh.antennapod.core.service.playback.PlaybackService;
 import de.danoeh.antennapod.service.PlayerWidgetService;
 
 public class PlayerWidget extends AppWidgetProvider {
-	private static final String TAG = "PlayerWidget";
+    private static final String TAG = "PlayerWidget";
 
     @Override
-	public void onReceive(Context context, Intent intent) {
-		if (StringUtils.equals(intent.getAction(), PlaybackService.FORCE_WIDGET_UPDATE)) {
-			startUpdate(context);
-		} else if (StringUtils.equals(intent.getAction(), PlaybackService.STOP_WIDGET_UPDATE)) {
-			stopUpdate(context);
-		}
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
+        super.onReceive(context, intent);
+        if (StringUtils.equals(intent.getAction(), PlaybackService.FORCE_WIDGET_UPDATE)) {
+            startUpdate(context);
+        } else if (StringUtils.equals(intent.getAction(), PlaybackService.STOP_WIDGET_UPDATE)) {
+            stopUpdate(context);
+        }
 
-	}
+    }
 
-	@Override
-	public void onEnabled(Context context) {
-		super.onEnabled(context);
-		if (BuildConfig.DEBUG)
-			Log.d(TAG, "Widget enabled");
-	}
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(TAG, "Widget enabled");
+    }
 
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-			int[] appWidgetIds) {
-		startUpdate(context);
-	}
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        Log.d(TAG, "onUpdate() called with: " + "context = [" + context + "], appWidgetManager = [" + appWidgetManager + "], appWidgetIds = [" + appWidgetIds + "]");
+        startUpdate(context);
+    }
 
-	private void startUpdate(Context context) {
-		context.startService(new Intent(context, PlayerWidgetService.class));
-	}
+    private void startUpdate(Context context) {
+        Log.d(TAG, "startUpdate() called with: " + "context = [" + context + "]");
+        context.startService(new Intent(context, PlayerWidgetService.class));
+    }
 
-	private void stopUpdate(Context context) {
-		context.stopService(new Intent(context, PlayerWidgetService.class));
-	}
+    private void stopUpdate(Context context) {
+        Log.d(TAG, "stopUpdate() called with: " + "context = [" + context + "]");
+        context.stopService(new Intent(context, PlayerWidgetService.class));
+    }
 
 }
