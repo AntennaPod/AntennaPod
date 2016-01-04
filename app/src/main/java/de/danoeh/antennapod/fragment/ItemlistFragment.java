840d065ecf4886f08a39381f8ccb3e9d9c0775cd
recalculated <recalculated@users.noreply.github.com>
2016-01-03 23:45:14 -0600

    Remove unnecessary TypedArray creation

--- app/src/main/java/de/danoeh/antennapod/fragment/ItemlistFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/ItemlistFragment.java
@@ -230,10 +230,6 @@ public class ItemlistFragment extends ListFragment {
                 menu.findItem(R.id.share_link_item).setVisible(false);
                 menu.findItem(R.id.visit_website_item).setVisible(false);
             }
-            int[] attrs = { R.attr.action_bar_icon_color };
-            TypedArray ta = getActivity().obtainStyledAttributes(UserPreferences.getTheme(), attrs);
-            int textColor = ta.getColor(0, Color.GRAY);
-            ta.recycle();
 
             isUpdatingFeed = MenuItemUtils.updateRefreshMenuItem(menu, R.id.refresh_item, updateRefreshMenuItemChecker);
         }
