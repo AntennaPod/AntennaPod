7b629b8579c91ed4ee4606a61aa2188c2acb41c7
Martin Fietz <Martin.Fietz@gmail.com>
2016-01-19 16:15:17 +0100

    Import OPML: Ask for permission to access external storage on Marshmallow

--- app/src/main/java/de/danoeh/antennapod/activity/OpmlImportFromPathActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/OpmlImportFromPathActivity.java
@@ -11,16 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
 import de.danoeh.antennapod.core.util.IntentUtils;
-import de.danoeh.antennapod.core.util.LangUtils;
 import de.danoeh.antennapod.core.util.StorageUtils;
 
 /**
@@ -114,19 +107,6 @@ public class OpmlImportFromPathActivity extends OpmlImportBaseActivity {
         }
     }
 
-    private void startImport(File file) {
-        Reader mReader = null;
-        try {
-            mReader = new InputStreamReader(new FileInputStream(file),
-                LangUtils.UTF_8);
-            Log.d(TAG, "Parsing " + file.toString());
-            startImport(mReader);
-        } catch (FileNotFoundException e) {
-            Log.d(TAG, "File not found which really should be there");
-            // this should never happen as it is a file we have just chosen
-        }
-    }
-
     /*
      * Creates an implicit intent to launch a file manager which lets
      * the user choose a specific OPML-file to import from.
@@ -155,13 +135,7 @@ public class OpmlImportFromPathActivity extends OpmlImportBaseActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == CHOOSE_OPML_FILE) {
             Uri uri = data.getData();
-
-            try {
-                Reader mReader = new InputStreamReader(getContentResolver().openInputStream(uri), LangUtils.UTF_8);
-                startImport(mReader);
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found");
-            }
+            importUri(uri);
         }
     }
 
