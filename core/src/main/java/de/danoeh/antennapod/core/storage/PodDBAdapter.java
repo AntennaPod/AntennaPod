b1212f8adb14c3afdbc8e6ec6a24f892805a740f
Martin Fietz <Martin.Fietz@gmail.com>
2016-01-30 17:51:25 +0100

    Fix warning

--- core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
+++ core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
@@ -1151,7 +1151,7 @@ public class PodDBAdapter {
      * The returned cursor uses the FEEDITEM_SEL_FI_SMALL selection.
      */
     public final Cursor getNewItemsCursor() {
-        String[] args = new String[] {
+        Object[] args = new String[] {
                 SEL_FI_SMALL_STR,
                 TABLE_NAME_FEED_ITEMS,
                 TABLE_NAME_FEEDS,
