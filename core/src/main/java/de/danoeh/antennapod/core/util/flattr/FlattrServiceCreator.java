748d2bc8634e6827e57f0b7bda9e3ef4bacabbcd
Domingos Lopes <domingos86lopes@gmail.com>
2016-03-22 02:12:59 -0400

    fix FlattrServiceCreator

--- core/src/main/java/de/danoeh/antennapod/core/util/flattr/FlattrServiceCreator.java
+++ core/src/main/java/de/danoeh/antennapod/core/util/flattr/FlattrServiceCreator.java
@@ -1,10 +1,12 @@
 package de.danoeh.antennapod.core.util.flattr;
 
 import android.util.Log;
-import de.danoeh.antennapod.core.BuildConfig;
+
 import org.shredzone.flattr4j.FlattrFactory;
 import org.shredzone.flattr4j.FlattrService;
 import org.shredzone.flattr4j.oauth.AccessToken;
+
+import de.danoeh.antennapod.core.BuildConfig;
 
 /** Ensures that only one instance of the FlattrService class exists at a time */
 
@@ -14,7 +16,10 @@ public class FlattrServiceCreator {
 	private static volatile FlattrService flattrService;
 	
 	public static FlattrService getService(AccessToken token) {
-		return FlattrFactory.getInstance().createFlattrService(token);
+		if (flattrService == null) {
+			flattrService = FlattrFactory.getInstance().createFlattrService(token);
+		}
+		return flattrService;
 	}
 	
 	public static void deleteFlattrService() {
