a16f2d2124f12e4c0a46afdbb588baa959983048
Martin Fietz <Martin.Fietz@gmail.com>
2016-06-01 12:13:13 +0200

    New episodes: Mark all as seen instead of played

--- app/src/main/java/de/danoeh/antennapod/fragment/NewEpisodesFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/NewEpisodesFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
