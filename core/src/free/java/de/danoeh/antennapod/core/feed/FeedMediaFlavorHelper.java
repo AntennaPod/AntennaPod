8425ac6190c34df1c3506a4d299f1838c8db69b4
Soso Tughushi <soso.tughush@devfactory.com>
2016-06-29 22:34:05 +0400

    Fixed rule 'Utility classes should not have public constructors'

--- core/src/free/java/de/danoeh/antennapod/core/feed/FeedMediaFlavorHelper.java
+++ core/src/free/java/de/danoeh/antennapod/core/feed/FeedMediaFlavorHelper.java
@@ -4,6 +4,7 @@ package de.danoeh.antennapod.core.feed;
  * Implements methods for FeedMedia that are flavor dependent.
  */
 public class FeedMediaFlavorHelper {
+    private FeedMediaFlavorHelper(){}
     static boolean instanceOfRemoteMedia(Object o) {
         return false;
     }
--- core/src/play/java/de/danoeh/antennapod/core/feed/FeedMediaFlavorHelper.java
+++ core/src/play/java/de/danoeh/antennapod/core/feed/FeedMediaFlavorHelper.java
@@ -6,6 +6,7 @@ import de.danoeh.antennapod.core.cast.RemoteMedia;
  * Implements methods for FeedMedia that are flavor dependent.
  */
 public class FeedMediaFlavorHelper {
+    private FeedMediaFlavorHelper(){}
     static boolean instanceOfRemoteMedia(Object o) {
         return o instanceof RemoteMedia;
     }
