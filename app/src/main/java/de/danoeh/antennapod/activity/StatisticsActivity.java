416117515c4e52727f91987e5b537256da34f09a
ByteHamster <info@bytehamster.com>
2016-08-28 18:35:05 +0200

    Allow choosing between getDuration and getPlayedDuration

--- app/src/main/java/de/danoeh/antennapod/activity/StatisticsActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/StatisticsActivity.java
@@ -1,14 +1,18 @@
 package de.danoeh.antennapod.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import de.danoeh.antennapod.R;
@@ -28,12 +32,16 @@ public class StatisticsActivity extends AppCompatActivity
         implements AdapterView.OnItemClickListener {
 
     private static final String TAG = StatisticsActivity.class.getSimpleName();
+    private static final String PREF_NAME = "StatisticsActivityPrefs";
+    private static final String PREF_COUNT_ALL = "countAll";
 
     private Subscription subscription;
     private TextView totalTimeTextView;
     private ListView feedStatisticsList;
     private ProgressBar progressBar;
     private StatisticsListAdapter listAdapter;
+    private boolean countAll = false;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +50,14 @@ public class StatisticsActivity extends AppCompatActivity
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setContentView(R.layout.statistics_activity);
 
+        prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        countAll = prefs.getBoolean(PREF_COUNT_ALL, false);
+
         totalTimeTextView = (TextView) findViewById(R.id.total_time);
         feedStatisticsList = (ListView) findViewById(R.id.statistics_list);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         listAdapter = new StatisticsListAdapter(this);
+        listAdapter.setCountAll(countAll);
         feedStatisticsList.setAdapter(listAdapter);
         feedStatisticsList.setOnItemClickListener(this);
     }
@@ -53,10 +65,15 @@ public class StatisticsActivity extends AppCompatActivity
     @Override
     public void onResume() {
         super.onResume();
-        progressBar.setVisibility(View.VISIBLE);
-        totalTimeTextView.setVisibility(View.GONE);
-        feedStatisticsList.setVisibility(View.GONE);
-        loadStats();
+        refreshStats();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.statistics, menu);
+        return true;
     }
 
     @Override
@@ -64,22 +81,54 @@ public class StatisticsActivity extends AppCompatActivity
         if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
+        } else if (item.getItemId() == R.id.calculation_type) {
+            selectCalculationType();
+            return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
     }
 
+    private void selectCalculationType() {
+        View contentView = View.inflate(this, R.layout.calculation_type, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(contentView);
+        builder.setTitle(R.string.calculation_type);
+
+        if (countAll) {
+            ((RadioButton) contentView.findViewById(R.id.calculation_type_count_all)).setChecked(true);
+        } else {
+            ((RadioButton) contentView.findViewById(R.id.calculation_type_normal)).setChecked(true);
+        }
+
+        builder.setPositiveButton(android.R.string.ok, (dialog, which) -> {
+            countAll = ((RadioButton) contentView.findViewById(R.id.calculation_type_count_all)).isChecked();
+            listAdapter.setCountAll(countAll);
+            prefs.edit().putBoolean(PREF_COUNT_ALL, countAll).commit();
+            refreshStats();
+        });
+
+        builder.show();
+    }
+
+    private void refreshStats() {
+        progressBar.setVisibility(View.VISIBLE);
+        totalTimeTextView.setVisibility(View.GONE);
+        feedStatisticsList.setVisibility(View.GONE);
+        loadStats();
+    }
+
     private void loadStats() {
-        if(subscription != null) {
+        if (subscription != null) {
             subscription.unsubscribe();
         }
-        subscription = Observable.fromCallable(() -> DBReader.getStatistics())
+        subscription = Observable.fromCallable(() -> DBReader.getStatistics(countAll))
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(result -> {
                     if (result != null) {
                         totalTimeTextView.setText(Converter
-                                .shortLocalizedDuration(this, result.totalTime));
+                                .shortLocalizedDuration(this, countAll ? result.totalTimeCountAll : result.totalTime));
                         listAdapter.update(result.feedTime);
                         progressBar.setVisibility(View.GONE);
                         totalTimeTextView.setVisibility(View.VISIBLE);
@@ -95,9 +144,10 @@ public class StatisticsActivity extends AppCompatActivity
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle(stats.feed.getTitle());
         dialog.setMessage(getString(R.string.statistics_details_dialog,
-                stats.episodesStarted,
+                countAll ? stats.episodesStartedIncludingMarked : stats.episodesStarted,
                 stats.episodes,
-                Converter.shortLocalizedDuration(this, stats.timePlayed),
+                Converter.shortLocalizedDuration(this, countAll ?
+                        stats.timePlayedCountAll : stats.timePlayed),
                 Converter.shortLocalizedDuration(this, stats.time)));
         dialog.setPositiveButton(android.R.string.ok, null);
         dialog.show();
