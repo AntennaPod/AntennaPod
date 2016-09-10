209058b3a7be15d8b3837ace29822eeb6d52960d
orionlee <orionlee@yahoo.com>
2016-09-10 12:04:01 -0700

    Make it explicit the lockscreen workaround is only relevant to pre Lollipop devices.

--- core/src/main/java/de/danoeh/antennapod/core/service/playback/PlaybackService.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/playback/PlaybackService.java
@@ -1004,9 +1004,14 @@ public class PlaybackService extends MediaBrowserServiceCompat {
         if (useSkipToPreviousForRewindInLockscreen()) {
             // Workaround to fool Android so that Lockscreen will expose a skip-to-previous button,
             // which will be used for rewind.
+            // The workaround is used for pre Lollipop (Androidv5) devices.
+            // For Androidv5+, lockscreen widges are really notifications (compact),
+            // with an independent codepath
             //
             // @see #sessionCallback in the backing callback, skipToPrevious implementation
-            // is actually the same as rewind. So no new inconsistency is created.
+            //   is actually the same as rewind. So no new inconsistency is created.
+            // @see #setupNotification() for the method to create Androidv5+ lockscreen UI
+            //   with notification (compact)
             capabilities = capabilities | PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS;
         }
 
@@ -1017,11 +1022,9 @@ public class PlaybackService extends MediaBrowserServiceCompat {
     private static boolean useSkipToPreviousForRewindInLockscreen() {
         // showRewindOnCompactNotification() corresponds to the "Set Lockscreen Buttons"
         // Settings in UI.
-        // Hence, from user perspective, he/she is setting the buttons for Loackscreen
-        //
-        // OPEN: it might contain other logic, e.g., the woakround might be applicable
-        // only to prev-Androidv5 devices.
-        return UserPreferences.showRewindOnCompactNotification();
+        // Hence, from user perspective, he/she is setting the buttons for Lockscreen
+        return ( UserPreferences.showRewindOnCompactNotification() &&
+                 (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) );
     }
 
     /**
