bde6e316fcdfe0d78cb17585a62d7aae70e371ab
ByteHamster <info@bytehamster.com>
2018-04-02 21:30:51 +0200

    Clarified test cases

--- app/src/androidTest/java/de/test/antennapod/feed/FeedItemTest.java
+++ app/src/androidTest/java/de/test/antennapod/feed/FeedItemTest.java
@@ -4,32 +4,34 @@ import android.test.AndroidTestCase;
 import de.danoeh.antennapod.core.feed.FeedItem;
 
 public class FeedItemTest extends AndroidTestCase {
+    private static final String TEXT_LONG = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    private static final String TEXT_SHORT = "Lorem ipsum";
 
-    public void testShownoteLength() throws Exception {
+    /**
+     * If one of `description` or `content:encoded` is null, use the other one.
+     */
+    public void testShownotesNullValues() throws Exception {
+        testShownotes(null, TEXT_LONG);
+        testShownotes(TEXT_LONG, null);
+    }
+
+    /**
+     * If `description` is reasonably longer than `content:encoded`, use `description`.
+     */
+    public void testShownotesLength() throws Exception {
+        testShownotes(TEXT_SHORT, TEXT_LONG);
+        testShownotes(TEXT_LONG, TEXT_SHORT);
+    }
+
+    /**
+     * Checks if the shownotes equal TEXT_LONG, using the given `description` and `content:encoded`
+     * @param description Description of the feed item
+     * @param contentEncoded `content:encoded` of the feed item
+     */
+    private void testShownotes(String description, String contentEncoded) throws Exception {
         FeedItem item = new FeedItem();
-
-        item.setDescription(null);
-        item.setContentEncoded("Hello world");
-        assertEquals("Hello world", item.loadShownotes().call());
-
-        item.setDescription("");
-        item.setContentEncoded("Hello world");
-        assertEquals("Hello world", item.loadShownotes().call());
-
-        item.setDescription("Hello world");
-        item.setContentEncoded(null);
-        assertEquals("Hello world", item.loadShownotes().call());
-
-        item.setDescription("Hello world");
-        item.setContentEncoded("");
-        assertEquals("Hello world", item.loadShownotes().call());
-
-        item.setDescription("Hi");
-        item.setContentEncoded("Hello world");
-        assertEquals("Hello world", item.loadShownotes().call());
-
-        item.setDescription("Hello world");
-        item.setContentEncoded("Hi");
-        assertEquals("Hello world", item.loadShownotes().call());
+        item.setDescription(description);
+        item.setContentEncoded(contentEncoded);
+        assertEquals(TEXT_LONG, item.loadShownotes().call());
     }
 }
