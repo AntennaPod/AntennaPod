4787e2ecc97c8da89af27f6db943e69c90484518
ByteHamster <info@bytehamster.com>
2018-05-06 20:07:35 +0200

    Fixed starting with DefaultActionButtonCallback

--- app/src/main/java/de/danoeh/antennapod/adapter/DefaultActionButtonCallback.java
+++ app/src/main/java/de/danoeh/antennapod/adapter/DefaultActionButtonCallback.java
@@ -83,13 +83,13 @@ public class DefaultActionButtonCallback implements ActionButtonCallback {
             } else { // media is downloaded
                 if (media.isCurrentlyPlaying()) {
                     new PlaybackServiceStarter(context, media)
-                            .startWhenPrepared(false)
+                            .startWhenPrepared(true)
                             .shouldStream(false)
                             .start();
                     context.sendBroadcast(new Intent(PlaybackService.ACTION_PAUSE_PLAY_CURRENT_EPISODE));
                 } else if (media.isCurrentlyPaused()) {
                     new PlaybackServiceStarter(context, media)
-                            .startWhenPrepared(false)
+                            .startWhenPrepared(true)
                             .shouldStream(false)
                             .start();
                     context.sendBroadcast(new Intent(PlaybackService.ACTION_RESUME_PLAY_CURRENT_EPISODE));
