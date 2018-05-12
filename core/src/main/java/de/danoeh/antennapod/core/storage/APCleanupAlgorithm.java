1bb0694403460aedf0bd7a2839fe5d4e19842a6b
orionlee <orionlee@yahoo.com>
2018-05-12 13:14:23 -0700

    Mark method only used in testing.

--- core/src/main/java/de/danoeh/antennapod/core/storage/APCleanupAlgorithm.java
+++ core/src/main/java/de/danoeh/antennapod/core/storage/APCleanupAlgorithm.java
@@ -110,6 +110,7 @@ public class APCleanupAlgorithm extends EpisodeCleanupAlgorithm {
         return getNumEpisodesToCleanup(0);
     }
 
+    @VisibleForTesting
     public float getNumberOfDaysAfterPlayback() { return numberOfDaysAfterPlayback; }
 
     private static Date minusDays(Date baseDate, float numberOfDays) {
