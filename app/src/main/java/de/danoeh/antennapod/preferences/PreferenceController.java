920f0613857f468804114fcd85da8733b71db9bd
ByteHamster <info@bytehamster.com>
2018-05-26 23:49:47 +0200

    Fixed build

--- app/src/main/java/de/danoeh/antennapod/preferences/PreferenceController.java
+++ app/src/main/java/de/danoeh/antennapod/preferences/PreferenceController.java
@@ -887,7 +887,7 @@ public class PreferenceController implements SharedPreferences.OnSharedPreferenc
 
     private void checkSonicItemVisibility() {
         if (Build.VERSION.SDK_INT < 16) {
-            MaterialListPreference p = ((MaterialListPreference) ui.findPreference(UserPreferences.PREF_MEDIA_PLAYER));
+            ListPreference p = (ListPreference) ui.findPreference(UserPreferences.PREF_MEDIA_PLAYER);
             p.setEntries(R.array.media_player_options_no_sonic);
             p.setEntryValues(R.array.media_player_values_no_sonic);
         }
