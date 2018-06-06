8fa31804466b1e958f561a991ac7ef1057cfcb3a
ByteHamster <info@bytehamster.com>
2018-06-06 15:08:46 +0200

    Restored tests

---
+++ core/src/test/java/de/danoeh/antennapod/core/feed/FeedItemMother.java
@@ -0,0 +1,16 @@
+package de.danoeh.antennapod.core.feed;
+
+import java.util.Date;
+
+import static de.danoeh.antennapod.core.feed.FeedMother.anyFeed;
+
+class FeedItemMother {
+    private static final String IMAGE_URL = "http://example.com/image";
+
+    static FeedItem anyFeedItemWithImage() {
+        FeedItem item = new FeedItem(0, "Item", "Item", "url", new Date(), FeedItem.PLAYED, anyFeed());
+        item.setImageUrl(IMAGE_URL);
+        return item;
+    }
+
+}
---
+++ core/src/test/java/de/danoeh/antennapod/core/feed/FeedMother.java
@@ -0,0 +1,12 @@
+package de.danoeh.antennapod.core.feed;
+
+class FeedMother {
+    public static final String IMAGE_URL = "http://example.com/image";
+
+    public static Feed anyFeed() {
+        return new Feed(0, null, "title", "http://example.com", "This is the description",
+                "http://example.com/payment", "Daniel", "en", null, "http://example.com/feed", IMAGE_URL,
+                null, "http://example.com/feed", true);
+    }
+
+}
