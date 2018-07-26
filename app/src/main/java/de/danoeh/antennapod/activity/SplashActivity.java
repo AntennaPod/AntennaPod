deb8a2eef32bbc0d56dd0eaec0d40d20bd8b0aa4
ByteHamster <info@bytehamster.com>
2018-07-26 09:25:21 +0200

    Added ProgressBar to splash screen

--- app/src/main/java/de/danoeh/antennapod/activity/SplashActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/SplashActivity.java
@@ -1,9 +1,16 @@
 package de.danoeh.antennapod.activity;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.storage.PodDBAdapter;
 
 /**
@@ -13,6 +20,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            Drawable wrapDrawable = DrawableCompat.wrap(progressBar.getIndeterminateDrawable());
+            DrawableCompat.setTint(wrapDrawable, 0xffffffff);
+            progressBar.setIndeterminateDrawable(DrawableCompat.unwrap(wrapDrawable));
+        } else {
+            progressBar.getIndeterminateDrawable().setColorFilter(0xffffffff, PorterDuff.Mode.SRC_IN);
+        }
 
         new Thread(() -> {
             // Trigger schema updates
