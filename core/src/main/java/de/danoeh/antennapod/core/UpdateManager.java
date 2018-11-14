eff2f154b52fec478890edf4716b0c4bb5f013a2
orionlee <orionlee@yahoo.com>
2018-11-14 14:53:51 -0800

    fix code formatting

--- core/src/main/java/de/danoeh/antennapod/core/UpdateManager.java
+++ core/src/main/java/de/danoeh/antennapod/core/UpdateManager.java
@@ -58,13 +58,13 @@ class UpdateManager {
     }
 
     private static void onUpgrade(final int oldVersionCode, final int newVersionCode) {
-        if(oldVersionCode < 1050004) {
+        if (oldVersionCode < 1050004) {
             if(MediaPlayer.isPrestoLibraryInstalled(context) && Build.VERSION.SDK_INT >= 16) {
                 UserPreferences.enableSonic();
             }
         }
 
-        if(oldVersionCode < 1070196) {
+        if (oldVersionCode < 1070196) {
             // migrate episode cleanup value (unit changed from days to hours)
             int oldValueInDays = UserPreferences.getEpisodeCleanupValue();
             if (oldValueInDays > 0) {
