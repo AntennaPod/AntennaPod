19d44d6d0d1f8020577adcff5e5b24abb7760b5c
Igor Almeida <igor.contato@gmail.com>
2019-02-03 18:24:57 -0200

    Use android-filepicker instead of intents

--- app/src/main/java/de/danoeh/antennapod/activity/AddLocalFilesActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/AddLocalFilesActivity.java
@@ -1,12 +1,10 @@
 package de.danoeh.antennapod.activity;
 
 import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,24 +17,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.angads25.filepicker.controller.DialogSelectionListener;
+import com.github.angads25.filepicker.model.DialogConfigs;
+import com.github.angads25.filepicker.model.DialogProperties;
+import com.github.angads25.filepicker.view.FilePickerDialog;
+
+import java.io.File;
 
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.feed.LocalFeedUpdater;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
-import de.danoeh.antennapod.core.util.IntentUtils;
 import de.danoeh.antennapod.core.util.StorageUtils;
 
 /**
  * Lets the user start the file import process from a path
  */
-public class AddLocalFilesActivity extends AppCompatActivity {
+public class AddLocalFilesActivity extends AppCompatActivity implements DialogSelectionListener {
 
     private static final String TAG = "AddLocalFilesAct";
     private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 5;
-
-    private static final int CHOOSE_SINGLE_FILE = 1;
-
-    private Intent intentPickAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +53,6 @@ public class AddLocalFilesActivity extends AppCompatActivity {
 
         int nextOption = 1;
         String optionLabel = getString(R.string.singlefile_import_option);
-        intentPickAction = new Intent(Intent.ACTION_PICK);
-
-        if (!IntentUtils.isCallable(getApplicationContext(), intentPickAction)) {
-            intentPickAction.setData(null);
-            if (!IntentUtils.isCallable(getApplicationContext(), intentPickAction)) {
-                singleFileHeaderExplanation1.setVisibility(View.GONE);
-                singleFileExplanation1.setVisibility(View.GONE);
-                findViewById(R.id.divider1).setVisibility(View.GONE);
-                butChooseFilesystem.setVisibility(View.GONE);
-            }
-        }
         if (singleFileExplanation1.getVisibility() == View.VISIBLE) {
             singleFileHeaderExplanation1.setText(String.format(optionLabel, nextOption));
             nextOption++;
@@ -104,8 +92,7 @@ public class AddLocalFilesActivity extends AppCompatActivity {
     }
 
     /*
-     * Creates an implicit intent to launch a file manager which lets
-     * the user choose a specific file to import.
+     * Lets the user choose a specific file to import.
      */
     private void chooseFileFromFilesystem() {
         int permission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE);
@@ -114,23 +101,27 @@ public class AddLocalFilesActivity extends AppCompatActivity {
             return;
         }
 
-        try {
-            startActivityForResult(intentPickAction, CHOOSE_SINGLE_FILE);
-        } catch (ActivityNotFoundException e) {
-            Log.e(TAG, "No activity found. Should never happen...");
-        }
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_SELECT;
+        properties.root = new File(DialogConfigs.DEFAULT_DIR);
+        properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
+        properties.offset = new File(DialogConfigs.DEFAULT_DIR);
+        properties.extensions = null;
+
+        FilePickerDialog dialog = new FilePickerDialog(this, properties);
+        dialog.setTitle("Select a File");
+
+        dialog.setDialogSelectionListener(this);
+
+        dialog.show();
     }
 
-    /**
-     * With the path, attempt to start the import process
-     */
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK && requestCode == CHOOSE_SINGLE_FILE) {
-            Uri uri = data.getData();
-            if (uri != null && uri.toString().startsWith("/")) {
-                uri = Uri.parse("file://" + uri.toString());
-            }
+    public void onSelectedFilePaths(String[] files) {
+        for (String f: files) {
+            Uri uri = new Uri.Builder().scheme("file").path(f).build();
+            Log.d(TAG, "path is " + uri.getPath());
             boolean ret = importUri(uri);
             if (ret == false) {
                 Toast.makeText(this, "Could not import", Toast.LENGTH_LONG).show();
@@ -140,8 +131,6 @@ public class AddLocalFilesActivity extends AppCompatActivity {
 
             //go back to main
             finish();
-        } else {
-            Log.d(TAG, "result not ok or code not file: " + resultCode + "," + requestCode);
         }
     }
 
@@ -158,8 +147,7 @@ public class AddLocalFilesActivity extends AppCompatActivity {
                     .show();
             return false;
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                uri.toString().contains(Environment.getExternalStorageDirectory().toString())) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             int permission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE);
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 return false;
