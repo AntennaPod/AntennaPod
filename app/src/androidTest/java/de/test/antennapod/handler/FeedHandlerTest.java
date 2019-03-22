f79d49adb1b5f3d5cc3f3724bce9f8f152256096
D-sha <deanshannon3@gmail.com>
2019-03-22 22:31:58 +1000

    Repair FeedHandlerTest
    Use InstrumentationRegistry to get test context
    Set image Url so it will match test feed

--- app/src/androidTest/java/de/test/antennapod/handler/FeedHandlerTest.java
+++ app/src/androidTest/java/de/test/antennapod/handler/FeedHandlerTest.java
@@ -1,6 +1,7 @@
 package de.test.antennapod.handler;
 
 import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.test.InstrumentationTestCase;
 
 import org.xml.sax.SAXException;
@@ -36,7 +37,7 @@ public class FeedHandlerTest extends InstrumentationTestCase {
 
     protected void setUp() throws Exception {
         super.setUp();
-        Context context = getInstrumentation().getContext();
+        Context context = InstrumentationRegistry.getTargetContext();
         File destDir = context.getExternalFilesDir(FEEDS_DIR);
         assertNotNull(destDir);
 
@@ -66,6 +67,7 @@ public class FeedHandlerTest extends InstrumentationTestCase {
         Feed parsedFeed = new Feed(feed.getDownload_url(), feed.getLastUpdate());
         parsedFeed.setFile_url(file.getAbsolutePath());
         parsedFeed.setDownloaded(true);
+        parsedFeed.setImageUrl("http://example.com/picture");
         handler.parseFeed(parsedFeed);
         return parsedFeed;
     }
