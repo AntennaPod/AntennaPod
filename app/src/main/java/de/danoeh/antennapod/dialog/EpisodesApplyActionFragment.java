cbe61a6815d2bfa6dfa8afeae9a879b380eadadc
orionlee <orionlee@yahoo.com>
2019-09-03 11:50:23 -0700

    #3374 fix batch edit FAB overlay remains when deselect all

--- app/src/main/java/de/danoeh/antennapod/dialog/EpisodesApplyActionFragment.java
+++ app/src/main/java/de/danoeh/antennapod/dialog/EpisodesApplyActionFragment.java
@@ -3,7 +3,6 @@ package de.danoeh.antennapod.dialog;
 import android.app.AlertDialog;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
@@ -13,7 +12,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.ArrayMap;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -218,7 +216,11 @@ public class EpisodesApplyActionFragment extends Fragment {
     }
 
     private void showSpeedDialIfAnyChecked() {
-        mSpeedDialView.setVisibility(checkedIds.size() > 0 ? View.VISIBLE : View.GONE);
+        if (checkedIds.size() > 0) {
+            mSpeedDialView.show();
+        } else {
+            mSpeedDialView.hide(); // hide() also handles UI, e.g., overlay properly.
+        }
     }
 
     @Override
