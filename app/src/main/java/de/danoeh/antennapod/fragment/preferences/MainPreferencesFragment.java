5f01e5045a9bf628f5c8d50b08936d8fe88a6b26
ByteHamster <info@bytehamster.com>
2019-09-11 23:29:58 +0200

    Added screen that shows crash info

--- app/src/main/java/de/danoeh/antennapod/fragment/preferences/MainPreferencesFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/preferences/MainPreferencesFragment.java
@@ -1,27 +1,20 @@
 package de.danoeh.antennapod.fragment.preferences;
 
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.util.Log;
 import android.widget.Toast;
 import com.bytehamster.lib.preferencesearch.SearchConfiguration;
 import com.bytehamster.lib.preferencesearch.SearchPreference;
-import de.danoeh.antennapod.CrashReportWriter;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.AboutActivity;
+import de.danoeh.antennapod.activity.CrashReportActivity;
 import de.danoeh.antennapod.activity.PreferenceActivity;
 import de.danoeh.antennapod.activity.StatisticsActivity;
-
-import java.util.List;
 
 public class MainPreferencesFragment extends PreferenceFragmentCompat {
     private static final String TAG = "MainPreferencesFragment";
@@ -87,26 +80,7 @@ public class MainPreferencesFragment extends PreferenceFragmentCompat {
             return true;
         });
         findPreference(PREF_SEND_CRASH_REPORT).setOnPreferenceClickListener(preference -> {
-            Context context = getActivity().getApplicationContext();
-            Intent emailIntent = new Intent(Intent.ACTION_SEND);
-            emailIntent.setType("text/plain");
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"Martin.Fietz@gmail.com"});
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "AntennaPod Crash Report");
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "Please describe what you were doing when the app crashed");
-            // the attachment
-            Uri fileUri = FileProvider.getUriForFile(context, context.getString(R.string.provider_authority),
-                    CrashReportWriter.getFile());
-            emailIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
-            emailIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            String intentTitle = getActivity().getString(R.string.send_email);
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
-                List<ResolveInfo> resInfoList = context.getPackageManager().queryIntentActivities(emailIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                for (ResolveInfo resolveInfo : resInfoList) {
-                    String packageName = resolveInfo.activityInfo.packageName;
-                    context.grantUriPermission(packageName, fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                }
-            }
-            getActivity().startActivity(Intent.createChooser(emailIntent, intentTitle));
+            startActivity(new Intent(getActivity(), CrashReportActivity.class));
             return true;
         });
     }
