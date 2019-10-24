007f1fbb87b5ff79b8253a23ce4feb314a5a3e68
orionlee <orionlee@yahoo.com>
2019-10-24 13:39:28 -0700

    Remove unused comparators after SortOrder refactoring

--- core/src/main/java/de/danoeh/antennapod/core/util/comparator/FeedItemPubdateComparator.java
+++ core/src/main/java/de/danoeh/antennapod/core/util/comparator/FeedItemPubdateComparator.java
@@ -1,15 +1,11 @@
 package de.danoeh.antennapod.core.util.comparator;
 
-import java.util.Collections;
 import java.util.Comparator;
 
 import de.danoeh.antennapod.core.feed.FeedItem;
 
 /** Compares the pubDate of two FeedItems for sorting*/
 public class FeedItemPubdateComparator implements Comparator<FeedItem> {
-
-	public static final Comparator<FeedItem> descending = new FeedItemPubdateComparator();
-	public static final Comparator<FeedItem> ascending = Collections.reverseOrder(new FeedItemPubdateComparator());
 
 	/** Returns a new instance of this comparator in reverse order.
 	public static FeedItemPubdateComparator newInstance() {
