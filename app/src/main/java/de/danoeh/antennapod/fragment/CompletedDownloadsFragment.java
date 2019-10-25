2326f712621064d881c1ba9d13cdccda6e5043ea
ByteHamster <info@bytehamster.com>
2019-10-25 11:29:35 +0200

    Fixed downloads fragment not updating

--- app/src/main/java/de/danoeh/antennapod/fragment/CompletedDownloadsFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/CompletedDownloadsFragment.java
@@ -17,6 +17,7 @@ import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.MainActivity;
 import de.danoeh.antennapod.adapter.DownloadedEpisodesListAdapter;
 import de.danoeh.antennapod.core.event.DownloadLogEvent;
+import de.danoeh.antennapod.core.event.UnreadItemsUpdateEvent;
 import de.danoeh.antennapod.core.feed.FeedItem;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.DBWriter;
@@ -140,6 +141,11 @@ public class CompletedDownloadsFragment extends ListFragment {
 
     @Subscribe
     public void onDownloadLogChanged(DownloadLogEvent event) {
+        loadItems();
+    }
+
+    @Subscribe
+    public void onUnreadItemsChanged(UnreadItemsUpdateEvent event) {
         loadItems();
     }
 
