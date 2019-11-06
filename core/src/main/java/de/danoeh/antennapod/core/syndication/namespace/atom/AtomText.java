d953692443c8e5ae03027210f8a7dcb306e4730d
ByteHamster <info@bytehamster.com>
2019-11-06 17:17:20 +0100

    Removed commons text library that was just used 2 times

--- core/src/main/java/de/danoeh/antennapod/core/syndication/namespace/atom/AtomText.java
+++ core/src/main/java/de/danoeh/antennapod/core/syndication/namespace/atom/AtomText.java
@@ -1,47 +1,51 @@
 package de.danoeh.antennapod.core.syndication.namespace.atom;
 
-import org.apache.commons.text.StringEscapeUtils;
-
+import android.os.Build;
+import android.text.Html;
 import de.danoeh.antennapod.core.syndication.namespace.Namespace;
 import de.danoeh.antennapod.core.syndication.namespace.SyndElement;
 
 /** Represents Atom Element which contains text (content, title, summary). */
 public class AtomText extends SyndElement {
-	public static final String TYPE_TEXT = "text";
-	private static final String TYPE_HTML = "html";
-	private static final String TYPE_XHTML = "xhtml";
+    public static final String TYPE_TEXT = "text";
+    public static final String TYPE_HTML = "html";
+    private static final String TYPE_XHTML = "xhtml";
 
-	private final String type;
-	private String content;
+    private final String type;
+    private String content;
 
-	public AtomText(String name, Namespace namespace, String type) {
-		super(name, namespace);
-		this.type = type;
-	}
+    public AtomText(String name, Namespace namespace, String type) {
+        super(name, namespace);
+        this.type = type;
+    }
 
-	/** Processes the content according to the type and returns it. */
-	public String getProcessedContent() {
-		if (type == null) {
-			return content;
-		} else if (type.equals(TYPE_HTML)) {
-			return StringEscapeUtils.unescapeHtml4(content);
-		} else if (type.equals(TYPE_XHTML)) {
-			return content;
-		} else { // Handle as text by default
-			return content;
-		}
-	}
+    /** Processes the content according to the type and returns it. */
+    public String getProcessedContent() {
+        if (type == null) {
+            return content;
+        } else if (type.equals(TYPE_HTML)) {
+            if (Build.VERSION.SDK_INT >= 24) {
+                return Html.fromHtml(content, Html.FROM_HTML_MODE_LEGACY).toString();
+            } else {
+                return Html.fromHtml(content).toString();
+            }
+        } else if (type.equals(TYPE_XHTML)) {
+            return content;
+        } else { // Handle as text by default
+            return content;
+        }
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
 }
