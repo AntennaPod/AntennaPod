fe632a4f9f5dd5aa9ed8e54f42aa1496ce08f480
ByteHamster <info@bytehamster.com>
2020-02-12 13:38:18 +0100

    Fixed other square images

--- app/src/main/java/de/danoeh/antennapod/view/SquareImageView.java
+++ app/src/main/java/de/danoeh/antennapod/view/SquareImageView.java
@@ -1,13 +1,16 @@
 package de.danoeh.antennapod.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import androidx.appcompat.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import de.danoeh.antennapod.core.R;
 
 /**
  * From http://stackoverflow.com/a/19449488/6839
  */
 public class SquareImageView extends AppCompatImageView {
+    private boolean useMinimum = false;
 
     public SquareImageView(Context context) {
         super(context);
@@ -15,17 +18,29 @@ public class SquareImageView extends AppCompatImageView {
 
     public SquareImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        loadAttrs(context, attrs);
     }
 
     public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        loadAttrs(context, attrs);
+    }
+
+    private void loadAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                new int[]{R.styleable.SquareImageView_useMinimum}, 0, 0);
+        useMinimum = a.getBoolean(0, false);
+        a.recycle();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        int size = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        int size = getMeasuredWidth();
+        if (useMinimum) {
+            size = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        }
         setMeasuredDimension(size, size);
     }
 
