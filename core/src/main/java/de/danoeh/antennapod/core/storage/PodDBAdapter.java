0dc1b522ce09eb3a92090ed9f549df50c50d187c
Tony Tam <engineer.tonytam@pm.me>
2020-03-28 23:49:25 -0700

    db upgrade

--- core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
+++ core/src/main/java/de/danoeh/antennapod/core/storage/PodDBAdapter.java
@@ -48,7 +48,7 @@ public class PodDBAdapter {
 
     private static final String TAG = "PodDBAdapter";
     public static final String DATABASE_NAME = "Antennapod.db";
-    public static final int VERSION = 1090000;
+    public static final int VERSION = 1091000;
 
     /**
      * Maximum number of arguments for IN-operator.
