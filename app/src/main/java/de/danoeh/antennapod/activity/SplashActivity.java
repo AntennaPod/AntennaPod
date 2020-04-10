4b42ed87c38de5b6419019d999f459d85ee056a6
ByteHamster <info@bytehamster.com>
2020-04-10 21:57:20 +0200

    Toast instead of crash on database downgrade

--- app/src/main/java/de/danoeh/antennapod/activity/SplashActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +15,11 @@ import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.storage.PodDBAdapter;
 import io.reactivex.Completable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 /**
- * Shows the AntennaPod logo while waiting for the main activity to start
+ * Shows the AntennaPod logo while waiting for the main activity to start.
  */
 public class SplashActivity extends AppCompatActivity {
     @Override
@@ -40,13 +42,17 @@ public class SplashActivity extends AppCompatActivity {
             PodDBAdapter.getInstance().close();
             subscriber.onComplete();
         })
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(() -> {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                overridePendingTransition(0, 0);
-                finish();
-            });
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(() -> {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(0, 0);
+                    finish();
+                }, error -> {
+                        error.printStackTrace();
+                        Toast.makeText(this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        finish();
+                    });
     }
 }
