f32a2ef8da6cc3c22faf66f2ac7649c751b55e2d
Hannes Achleitner <hannes.software@gmx.at>
2020-04-30 23:15:32 +0200

    run tests just once

--- .github/workflows/runTests.sh
+++
@@ -1,8 +0,0 @@
-runTests() {
-    ./gradlew connectedPlayDebugAndroidTest \
-        -Pandroid.testInstrumentationRunnerArguments.notAnnotation=de.test.antennapod.IgnoreOnCi \
-        | grep -v "V/InstrumentationResultParser: INSTRUMENTATION_STATUS"
-}
-
-# Retry tests to make them less flaky
-runTests || runTests || runTests
