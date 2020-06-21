7de9e0d4c1807bff054c1aef999fc600088ab521
ByteHamster <info@bytehamster.com>
2020-06-21 12:49:02 +0200

    Using state lists for colors

--- app/src/main/java/de/danoeh/antennapod/dialog/FilterDialog.java
+++ app/src/main/java/de/danoeh/antennapod/dialog/FilterDialog.java
@@ -1,22 +1,18 @@
 package de.danoeh.antennapod.dialog;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RelativeLayout;
-
 import androidx.appcompat.app.AlertDialog;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.feed.FeedItemFilter;
 import de.danoeh.antennapod.core.feed.FeedItemFilterGroup;
+import de.danoeh.antennapod.view.RecursiveRadioGroup;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class FilterDialog {
 
@@ -35,91 +31,35 @@ public abstract class FilterDialog {
         builder.setTitle(R.string.filter);
 
         LayoutInflater inflater = LayoutInflater.from(this.context);
-        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.filter_dialog_layout, null, false);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.filter_dialog, null, false);
         builder.setView(layout);
 
-        for (FeedItemFilterGroup.FeedItemEnum item : FeedItemFilterGroup.FeedItemEnum.values()) {
-
-            RelativeLayout row = (RelativeLayout) inflater.inflate(R.layout.filter_dialog_relative_cardview, null);
-            RadioButton radioButton1 = row.findViewById(R.id.filter_dialog_radioButton1);
-            RadioButton radioButton2 = row.findViewById(R.id.filter_dialog_radioButton2);
-            RadioButton radioButton3 = row.findViewById(R.id.filter_dialog_radioButton3);
-            radioButton1.setText(item.values[1].displayName);
-            radioButton1.setTextColor(Color.BLACK);
-            radioButton2.setText(item.values[0].displayName);
-            radioButton2.setTextColor(Color.BLACK);
-
-            Iterator<String> filterIterator = filterValues.iterator();
-            while (filterIterator.hasNext()) {
-                String nextItem = filterIterator.next();
-                if (item.values[1].filterId.equals(nextItem)) {
-                    item.values[1].setSelected(true);
-                    item.values[0].setSelected(false);
-                    radioButton1.setBackgroundResource(R.color.accent_light);
-                    radioButton2.setBackgroundResource(R.color.master_switch_background_light);
-                    radioButton3.setBackgroundResource(R.drawable.filter_dialog_x_on);
-                    radioButton1.setSelected(true);
-                    radioButton2.setSelected(false);
-                    radioButton1.setTextColor(Color.WHITE);
-                    radioButton2.setTextColor(Color.BLACK);
-                }
-                if (item.values[0].filterId.equals(nextItem)) {
-                    item.values[0].setSelected(true);
-                    item.values[1].setSelected(false);
-                    radioButton2.setBackgroundResource(R.color.accent_light);
-                    radioButton1.setBackgroundResource(R.color.master_switch_background_light);
-                    radioButton3.setBackgroundResource(R.drawable.filter_dialog_x_on);
-                    radioButton2.setSelected(true);
-                    radioButton1.setSelected(false);
-                    radioButton2.setTextColor(Color.WHITE);
-                    radioButton1.setTextColor(Color.BLACK);
-                }
-            }
-
-            radioButton1.setOnClickListener(arg0 -> {
-                item.values[1].setSelected(true);
-                item.values[0].setSelected(false);
-                radioButton1.setBackgroundResource(R.color.accent_light);
-                radioButton2.setBackgroundResource(R.color.master_switch_background_light);
-                radioButton3.setBackgroundResource(R.drawable.filter_dialog_x_on);
-                radioButton2.setSelected(false);
-                radioButton2.setTextColor(Color.BLACK);
-                radioButton1.setSelected(true);
-                radioButton1.setTextColor(Color.WHITE);
-            });
-            radioButton2.setOnClickListener(arg0 -> {
-                item.values[0].setSelected(true);
-                item.values[1].setSelected(false);
-                radioButton2.setBackgroundResource(R.color.accent_light);
-                radioButton1.setBackgroundResource(R.color.master_switch_background_light);
-                radioButton3.setBackgroundResource(R.drawable.filter_dialog_x_on);
-                radioButton1.setSelected(false);
-                radioButton1.setTextColor(Color.BLACK);
-                radioButton2.setSelected(true);
-                radioButton2.setTextColor(Color.WHITE);
-            });
-            radioButton3.setOnClickListener(arg0 -> {
-                item.values[0].setSelected(false);
-                item.values[1].setSelected(false);
-                radioButton1.setBackgroundResource(R.color.master_switch_background_light);
-                radioButton2.setBackgroundResource(R.color.master_switch_background_light);
-                radioButton3.setBackgroundResource(R.drawable.filter_dialog_x_off);
-                radioButton2.setTextColor(Color.BLACK);
-                radioButton2.setSelected(false);
-                radioButton1.setTextColor(Color.BLACK);
-                radioButton1.setSelected(false);
-            });
+        for (FeedItemFilterGroup item : FeedItemFilterGroup.values()) {
+            RecursiveRadioGroup row = (RecursiveRadioGroup) inflater.inflate(R.layout.filter_dialog_row, null);
+            RadioButton filter1 = row.findViewById(R.id.filter_dialog_radioButton1);
+            RadioButton filter2 = row.findViewById(R.id.filter_dialog_radioButton2);
+            filter1.setText(item.values[0].displayName);
+            filter1.setTag(item.values[0].filterId);
+            filter2.setText(item.values[1].displayName);
+            filter2.setTag(item.values[1].filterId);
             layout.addView(row);
         }
 
+        for (String filterId : filterValues) {
+            if (!TextUtils.isEmpty(filterId)) {
+                ((RadioButton) layout.findViewWithTag(filterId)).setChecked(true);
+            }
+        }
 
         builder.setPositiveButton(R.string.confirm_label, (dialog, which) -> {
             filterValues.clear();
-            for (FeedItemFilterGroup.FeedItemEnum item : FeedItemFilterGroup.FeedItemEnum.values()) {
-                for (int i = 0; i < item.values.length; i++) {
-                    if (item.values[i].getSelected()) {
-                        filterValues.add(item.values[i].filterId);
-                    }
+            for (int i = 0; i < layout.getChildCount(); i++) {
+                if (!(layout.getChildAt(i) instanceof RecursiveRadioGroup)) {
+                    continue;
+                }
+                RecursiveRadioGroup group = (RecursiveRadioGroup) layout.getChildAt(i);
+                if (group.getCheckedButton() != null) {
+                    filterValues.add((String) group.getCheckedButton().getTag());
                 }
             }
             updateFilter(filterValues);
--- core/src/main/java/de/danoeh/antennapod/core/feed/FeedItemFilterGroup.java
+++ core/src/main/java/de/danoeh/antennapod/core/feed/FeedItemFilterGroup.java
@@ -2,83 +2,35 @@ package de.danoeh.antennapod.core.feed;
 
 import de.danoeh.antennapod.core.R;
 
-public class FeedItemFilterGroup {
+public enum FeedItemFilterGroup {
+    PLAYED(new ItemProperties(R.string.hide_played_episodes_label, "played"),
+            new ItemProperties(R.string.not_played, "unplayed")),
+    PAUSED(new ItemProperties(R.string.hide_paused_episodes_label, "paused"),
+            new ItemProperties(R.string.not_paused, "")),
+    FAVORITE(new ItemProperties(R.string.hide_is_favorite_label, "is_favorite"),
+            new ItemProperties(R.string.not_favorite, "")),
+    MEDIA(new ItemProperties(R.string.has_media, ""),
+            new ItemProperties(R.string.no_media, "no_media")),
+    QUEUED(new ItemProperties(R.string.queued_label, "queued"),
+            new ItemProperties(R.string.not_queued_label, "not_queued")),
+    DOWNLOADED(new ItemProperties(R.string.hide_downloaded_episodes_label, "downloaded"),
+            new ItemProperties(R.string.hide_not_downloaded_episodes_label, "not_downloaded"));
 
-    private static boolean DEFAULT_SELECTED_STATE = false;
+    public final ItemProperties[] values;
 
-    private static final String NO_FILTERID = "";
-    private static final int HASMEDIA_LABEL = R.string.has_media;
-    private static final int NOTPAUSED_LABEL = R.string.not_paused;
-    private static final int NOTFAVORITE_LABEL = R.string.not_favorite;
+    FeedItemFilterGroup(ItemProperties... values) {
+        this.values = values;
+    }
 
-    private static final int UNPLAYED_LABEL = R.string.not_played;
-    private static final String UNPLAYED_FILTERID = "unplayed";
+    public static class ItemProperties {
 
-    private static final int PLAYED_LABEL = R.string.hide_played_episodes_label;
-    private static final String PLAYED_FILTERID = "played";
+        public final int displayName;
+        public final String filterId;
 
-    private static final int PAUSED_LABEL = R.string.hide_paused_episodes_label;
-    private static final String PAUSED_FILTERID = "paused";
-
-    private static final int ISFAVORITE_LABEL = R.string.hide_is_favorite_label;
-    private static final String ISFAVORITE_FILTERID = "is_favorite";
-
-    private static final int NOMEDIA_LABEL = R.string.no_media;
-    private static final String NOMEDIA_FILTERID = "no_media";
-
-    private static final int QUEUED_LABEL = R.string.queue_label;
-    private static final String QUEUED_FILTERID = "queued";
-
-    private static final int NOTQUEUED_LABEL = R.string.not_queued_label;
-    private static final String NOTQUEUED_FILTERID = "not_queued";
-
-    private static final int NOTDOWNLOADED_LABEL = R.string.hide_downloaded_episodes_label;
-    private static final String NOTDOWNLOADED_FILTERID = "not_downloaded";
-
-    private static final int DOWNLOADED_LABEL = R.string.hide_downloaded_episodes_label;
-    private static final String DOWNLOADED_FILTERID = "downloaded";
-
-    public enum FeedItemEnum {
-
-        PLAYED(new ItemProperties(DEFAULT_SELECTED_STATE, UNPLAYED_LABEL, UNPLAYED_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, PLAYED_LABEL, PLAYED_FILTERID)),
-        PAUSED(new ItemProperties(DEFAULT_SELECTED_STATE, NOTPAUSED_LABEL, NO_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, PAUSED_LABEL, PAUSED_FILTERID)),
-        FAVORITE(new ItemProperties(DEFAULT_SELECTED_STATE, NOTFAVORITE_LABEL, NO_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, ISFAVORITE_LABEL, ISFAVORITE_FILTERID)),
-        MEDIA(new ItemProperties(DEFAULT_SELECTED_STATE, NOMEDIA_LABEL, NOMEDIA_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, HASMEDIA_LABEL, NO_FILTERID)),
-        QUEUED(new ItemProperties(DEFAULT_SELECTED_STATE, NOTQUEUED_LABEL, NOTQUEUED_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, QUEUED_LABEL, QUEUED_FILTERID)),
-        DOWNLOADED(new ItemProperties(DEFAULT_SELECTED_STATE, NOTDOWNLOADED_LABEL, NOTDOWNLOADED_FILTERID),
-                new ItemProperties(DEFAULT_SELECTED_STATE, DOWNLOADED_LABEL, DOWNLOADED_FILTERID));
-
-        public final ItemProperties[] values;
-
-        FeedItemEnum(ItemProperties... values) {
-            this.values = values;
+        public ItemProperties(int displayName, String filterId) {
+            this.displayName = displayName;
+            this.filterId = filterId;
         }
 
-        public static class ItemProperties {
-
-            public final int displayName;
-            public boolean selected;
-            public final String filterId;
-
-            public void setSelected(boolean value) {
-                this.selected = value;
-            }
-
-            public boolean getSelected() {
-                return this.selected;
-            }
-
-            public ItemProperties(boolean selected, int displayName, String filterId) {
-                this.selected = selected;
-                this.displayName = displayName;
-                this.filterId = filterId;
-            }
-
-        }
     }
 }
