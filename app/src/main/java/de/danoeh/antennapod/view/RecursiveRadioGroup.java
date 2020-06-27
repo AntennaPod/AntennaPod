a4ce162491cb93535ee1a03d526c8d1228d58fd2
bws9000 <armorsoft@gmail.com>
2020-06-27 10:24:26 -0400

    Redesign filter dialog

--- app/src/main/java/de/danoeh/antennapod/view/RecursiveRadioGroup.java
+++ app/src/main/java/de/danoeh/antennapod/view/RecursiveRadioGroup.java
@@ -2,7 +2,6 @@ package de.danoeh.antennapod.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.ActionMode;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
