148b86a3e84a65cade4765c6224918f57109b5b3
ByteHamster <info@bytehamster.com>
2020-07-01 10:05:27 +0200

    Removed DirectoryChooserActivity
    This lead to problems when removing the sd card and left the app in stuck mode.
    Selecting a folder freely that way will not work with new Android versions anyway.
    
    Now, AntennaPod shows the folder chooser that is displayed on the storage settings screen.

--- app/src/main/java/de/danoeh/antennapod/activity/StorageErrorActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/StorageErrorActivity.java
@@ -1,37 +1,24 @@
 package de.danoeh.antennapod.activity;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
-
-import java.io.File;
-
+import androidx.appcompat.app.AppCompatActivity;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
 import de.danoeh.antennapod.core.util.StorageUtils;
 import de.danoeh.antennapod.dialog.ChooseDataFolderDialog;
 
-/** Is show if there is now external storage available. */
+/**
+ * Is show if there is now external storage available.
+ */
 public class StorageErrorActivity extends AppCompatActivity {
-
     private static final String TAG = "StorageErrorActivity";
-
-    private static final String[] EXTERNAL_STORAGE_PERMISSIONS = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE };
-    private static final int PERMISSION_REQUEST_EXTERNAL_STORAGE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,51 +28,7 @@ public class StorageErrorActivity extends AppCompatActivity {
         setContentView(R.layout.storage_error);
 
         Button btnChooseDataFolder = findViewById(R.id.btnChooseDataFolder);
-        btnChooseDataFolder.setOnClickListener(v -> {
-            if (Build.VERSION_CODES.KITKAT <= Build.VERSION.SDK_INT &&
-                    Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
-                showChooseDataFolderDialog();
-            } else {
-                openDirectoryChooser();
-            }
-        });
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int readPermission = ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE);
-            int writePermission = ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            if (readPermission != PackageManager.PERMISSION_GRANTED ||
-                    writePermission != PackageManager.PERMISSION_GRANTED) {
-                requestPermission();
-            }
-        }
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(this, EXTERNAL_STORAGE_PERMISSIONS,
-                PERMISSION_REQUEST_EXTERNAL_STORAGE);
-    }
-
-    private void openDirectoryChooser() {
-        Intent intent = new Intent(this, DirectoryChooserActivity.class);
-        startActivityForResult(intent, DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions,
-                                           int[] grantResults) {
-        if (requestCode != PERMISSION_REQUEST_EXTERNAL_STORAGE || grantResults.length != 2) {
-            return;
-        }
-        if (grantResults[0] != PackageManager.PERMISSION_GRANTED ||
-                grantResults[1] != PackageManager.PERMISSION_GRANTED) {
-            new AlertDialog.Builder(this)
-                    .setMessage(R.string.choose_data_directory_permission_rationale)
-                    .setPositiveButton(android.R.string.ok, (dialog, which) -> requestPermission())
-                    .setNegativeButton(android.R.string.cancel, (dialog, which) -> finish())
-                    .show();
-        }
+        btnChooseDataFolder.setOnClickListener(v -> showChooseDataFolderDialog());
     }
 
     @Override
@@ -108,7 +51,7 @@ public class StorageErrorActivity extends AppCompatActivity {
         }
     }
 
-    // see PreferenceController.showChooseDataFolderDialog()
+    // see StoragePreferencesFragment.showChooseDataFolderDialog()
     private void showChooseDataFolderDialog() {
         ChooseDataFolderDialog.showDialog(
                 this, new ChooseDataFolderDialog.RunnableWithString() {
@@ -118,43 +61,6 @@ public class StorageErrorActivity extends AppCompatActivity {
                         leaveErrorState();
                     }
                 });
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK
-                && requestCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
-            String dir = data.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR);
-
-            File path;
-            if (dir != null) {
-                path = new File(dir);
-            } else {
-                path = getExternalFilesDir(null);
-            }
-            if (path == null) {
-                return;
-            }
-            String message = null;
-            if (!path.exists()) {
-                message = String.format(getString(R.string.folder_does_not_exist_error), dir);
-            } else if (!path.canRead()) {
-                message = String.format(getString(R.string.folder_not_readable_error), dir);
-            } else if (!path.canWrite()) {
-                message = String.format(getString(R.string.folder_not_writable_error), dir);
-            }
-
-            if (message == null) {
-                Log.d(TAG, "Setting data folder: " + dir);
-                UserPreferences.setDataFolder(dir);
-                leaveErrorState();
-            } else {
-                AlertDialog.Builder ab = new AlertDialog.Builder(this);
-                ab.setMessage(message);
-                ab.setPositiveButton(android.R.string.ok, null);
-                ab.show();
-            }
-        }
     }
 
     private void leaveErrorState() {
