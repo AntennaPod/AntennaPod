d0211a84ef5f75ae3c3120fa924cacc49206f00c
asdoi <asdoi@t-online.de>
2020-07-20 14:52:13 +0200

    clickable filter message

--- app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
@@ -2,13 +2,19 @@ package de.danoeh.antennapod.fragment;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.LightingColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -454,6 +460,222 @@ public class FeedItemlistFragment extends Fragment implements AdapterView.OnItem
                 }
                 txtvInformation.setText("{fa-info-circle} " + this.getString(R.string.filtered_label));
                 Iconify.addIcons(txtvInformation);
+                txtvInformation.setOnClickListener((l) -> {
+                    try {
+                        MenuItem filterMenuItem = new MenuItem() {
+                            @Override
+                            public int getItemId() {
+                                return R.id.filter_items;
+                            }
+
+                            @Override
+                            public int getGroupId() {
+                                return 0;
+                            }
+
+                            @Override
+                            public int getOrder() {
+                                return 0;
+                            }
+
+                            @Override
+                            public MenuItem setTitle(CharSequence charSequence) {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setTitle(int i) {
+                                return null;
+                            }
+
+                            @Override
+                            public CharSequence getTitle() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setTitleCondensed(CharSequence charSequence) {
+                                return null;
+                            }
+
+                            @Override
+                            public CharSequence getTitleCondensed() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setIcon(Drawable drawable) {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setIcon(int i) {
+                                return null;
+                            }
+
+                            @Override
+                            public Drawable getIcon() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setIntent(Intent intent) {
+                                return null;
+                            }
+
+                            @Override
+                            public Intent getIntent() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setShortcut(char c, char c1) {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setNumericShortcut(char c) {
+                                return null;
+                            }
+
+                            @Override
+                            public char getNumericShortcut() {
+                                return 0;
+                            }
+
+                            @Override
+                            public MenuItem setAlphabeticShortcut(char c) {
+                                return null;
+                            }
+
+                            @Override
+                            public char getAlphabeticShortcut() {
+                                return 0;
+                            }
+
+                            @Override
+                            public MenuItem setCheckable(boolean b) {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean isCheckable() {
+                                return false;
+                            }
+
+                            @Override
+                            public MenuItem setChecked(boolean b) {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean isChecked() {
+                                return false;
+                            }
+
+                            @Override
+                            public MenuItem setVisible(boolean b) {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean isVisible() {
+                                return false;
+                            }
+
+                            @Override
+                            public MenuItem setEnabled(boolean b) {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean isEnabled() {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean hasSubMenu() {
+                                return false;
+                            }
+
+                            @Override
+                            public SubMenu getSubMenu() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setOnMenuItemClickListener(OnMenuItemClickListener onMenuItemClickListener) {
+                                return null;
+                            }
+
+                            @Override
+                            public ContextMenu.ContextMenuInfo getMenuInfo() {
+                                return null;
+                            }
+
+                            @Override
+                            public void setShowAsAction(int i) {
+
+                            }
+
+                            @Override
+                            public MenuItem setShowAsActionFlags(int i) {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setActionView(View view) {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setActionView(int i) {
+                                return null;
+                            }
+
+                            @Override
+                            public View getActionView() {
+                                return null;
+                            }
+
+                            @Override
+                            public MenuItem setActionProvider(ActionProvider actionProvider) {
+                                return null;
+                            }
+
+                            @Override
+                            public ActionProvider getActionProvider() {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean expandActionView() {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean collapseActionView() {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean isActionViewExpanded() {
+                                return false;
+                            }
+
+                            @Override
+                            public MenuItem setOnActionExpandListener(OnActionExpandListener onActionExpandListener) {
+                                return null;
+                            }
+                        };
+                        FeedMenuHandler.onOptionsItemClicked(getActivity(), filterMenuItem, feed);
+                    } catch (DownloadRequestException ignore) {
+                    }
+                });
+                //SelectableItemBackground but makes background transparent
+//                TypedValue outValue = new TypedValue();
+//                requireContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+//                txtvInformation.setBackgroundResource(outValue.resourceId);
                 txtvInformation.setVisibility(View.VISIBLE);
             } else {
                 txtvInformation.setVisibility(View.GONE);
