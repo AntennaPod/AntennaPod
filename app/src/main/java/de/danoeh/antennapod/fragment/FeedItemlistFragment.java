90b390c1e630fdf9f00e0de55f38c9e2a1527db0
asdoi <asdoi@t-online.de>
2020-07-21 13:32:37 +0200

    update filter dialog show

--- app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
@@ -58,6 +58,7 @@ import de.danoeh.antennapod.core.glide.FastBlurTransformation;
 import de.danoeh.antennapod.core.service.download.DownloadService;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.DBTasks;
+import de.danoeh.antennapod.core.storage.DBWriter;
 import de.danoeh.antennapod.core.storage.DownloadRequestException;
 import de.danoeh.antennapod.core.storage.DownloadRequester;
 import de.danoeh.antennapod.core.util.FeedItemPermutors;
@@ -66,6 +67,7 @@ import de.danoeh.antennapod.core.util.Optional;
 import de.danoeh.antennapod.core.util.ThemeUtils;
 import de.danoeh.antennapod.core.util.gui.MoreContentListFooterUtil;
 import de.danoeh.antennapod.dialog.EpisodesApplyActionFragment;
+import de.danoeh.antennapod.dialog.FilterDialog;
 import de.danoeh.antennapod.dialog.RenameFeedDialog;
 import de.danoeh.antennapod.menuhandler.FeedItemMenuHandler;
 import de.danoeh.antennapod.menuhandler.FeedMenuHandler;
@@ -83,6 +85,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Displays a list of FeedItems.
@@ -461,216 +464,15 @@ public class FeedItemlistFragment extends Fragment implements AdapterView.OnItem
                 txtvInformation.setText("{fa-info-circle} " + this.getString(R.string.filtered_label));
                 Iconify.addIcons(txtvInformation);
                 txtvInformation.setOnClickListener((l) -> {
-                    try {
-                        MenuItem filterMenuItem = new MenuItem() {
-                            @Override
-                            public int getItemId() {
-                                return R.id.filter_items;
-                            }
+                    FilterDialog filterDialog = new FilterDialog(requireContext(), feed.getItemFilter()) {
+                        @Override
+                        protected void updateFilter(Set<String> filterValues) {
+                            feed.setItemFilter(filterValues.toArray(new String[0]));
+                            DBWriter.setFeedItemsFilter(feed.getId(), filterValues);
+                        }
+                    };
 
-                            @Override
-                            public int getGroupId() {
-                                return 0;
-                            }
-
-                            @Override
-                            public int getOrder() {
-                                return 0;
-                            }
-
-                            @Override
-                            public MenuItem setTitle(CharSequence charSequence) {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setTitle(int i) {
-                                return null;
-                            }
-
-                            @Override
-                            public CharSequence getTitle() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setTitleCondensed(CharSequence charSequence) {
-                                return null;
-                            }
-
-                            @Override
-                            public CharSequence getTitleCondensed() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setIcon(Drawable drawable) {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setIcon(int i) {
-                                return null;
-                            }
-
-                            @Override
-                            public Drawable getIcon() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setIntent(Intent intent) {
-                                return null;
-                            }
-
-                            @Override
-                            public Intent getIntent() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setShortcut(char c, char c1) {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setNumericShortcut(char c) {
-                                return null;
-                            }
-
-                            @Override
-                            public char getNumericShortcut() {
-                                return 0;
-                            }
-
-                            @Override
-                            public MenuItem setAlphabeticShortcut(char c) {
-                                return null;
-                            }
-
-                            @Override
-                            public char getAlphabeticShortcut() {
-                                return 0;
-                            }
-
-                            @Override
-                            public MenuItem setCheckable(boolean b) {
-                                return null;
-                            }
-
-                            @Override
-                            public boolean isCheckable() {
-                                return false;
-                            }
-
-                            @Override
-                            public MenuItem setChecked(boolean b) {
-                                return null;
-                            }
-
-                            @Override
-                            public boolean isChecked() {
-                                return false;
-                            }
-
-                            @Override
-                            public MenuItem setVisible(boolean b) {
-                                return null;
-                            }
-
-                            @Override
-                            public boolean isVisible() {
-                                return false;
-                            }
-
-                            @Override
-                            public MenuItem setEnabled(boolean b) {
-                                return null;
-                            }
-
-                            @Override
-                            public boolean isEnabled() {
-                                return false;
-                            }
-
-                            @Override
-                            public boolean hasSubMenu() {
-                                return false;
-                            }
-
-                            @Override
-                            public SubMenu getSubMenu() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setOnMenuItemClickListener(OnMenuItemClickListener onMenuItemClickListener) {
-                                return null;
-                            }
-
-                            @Override
-                            public ContextMenu.ContextMenuInfo getMenuInfo() {
-                                return null;
-                            }
-
-                            @Override
-                            public void setShowAsAction(int i) {
-
-                            }
-
-                            @Override
-                            public MenuItem setShowAsActionFlags(int i) {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setActionView(View view) {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setActionView(int i) {
-                                return null;
-                            }
-
-                            @Override
-                            public View getActionView() {
-                                return null;
-                            }
-
-                            @Override
-                            public MenuItem setActionProvider(ActionProvider actionProvider) {
-                                return null;
-                            }
-
-                            @Override
-                            public ActionProvider getActionProvider() {
-                                return null;
-                            }
-
-                            @Override
-                            public boolean expandActionView() {
-                                return false;
-                            }
-
-                            @Override
-                            public boolean collapseActionView() {
-                                return false;
-                            }
-
-                            @Override
-                            public boolean isActionViewExpanded() {
-                                return false;
-                            }
-
-                            @Override
-                            public MenuItem setOnActionExpandListener(OnActionExpandListener onActionExpandListener) {
-                                return null;
-                            }
-                        };
-                        FeedMenuHandler.onOptionsItemClicked(getActivity(), filterMenuItem, feed);
-                    } catch (DownloadRequestException ignore) {
-                    }
+                    filterDialog.openDialog();
                 });
                 //SelectableItemBackground but makes background transparent
 //                TypedValue outValue = new TypedValue();
