0090f9d55e3cd8e11eaee1aa650245365e77fefc
Connectety-T <connectetyGit@yandex.com>
2020-10-07 15:12:18 +0200

    add intent for opening feed on clicking the notification.

--- core/src/main/java/de/danoeh/antennapod/core/service/FeedUpdateWorker.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/FeedUpdateWorker.java
@@ -4,14 +4,12 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import androidx.annotation.NonNull;
-
 import android.content.Intent;
 import android.content.res.Resources;
-import android.database.Cursor;
 import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.work.Worker;
@@ -28,18 +26,10 @@ import de.danoeh.antennapod.core.feed.Feed;
 import de.danoeh.antennapod.core.feed.FeedItem;
 import de.danoeh.antennapod.core.feed.FeedPreferences;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
-import de.danoeh.antennapod.core.service.download.DownloadStatus;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.DBTasks;
-import de.danoeh.antennapod.core.storage.DBWriter;
-import de.danoeh.antennapod.core.storage.DownloadRequestException;
-import de.danoeh.antennapod.core.storage.PodDBAdapter;
-import de.danoeh.antennapod.core.util.DownloadError;
-import de.danoeh.antennapod.core.util.LongIntMap;
 import de.danoeh.antennapod.core.util.NetworkUtils;
 import de.danoeh.antennapod.core.util.download.AutoUpdateManager;
-
-import static de.danoeh.antennapod.core.storage.DBReader.getFeed;
 
 public class FeedUpdateWorker extends Worker {
 
@@ -63,7 +53,7 @@ public class FeedUpdateWorker extends Worker {
             List<Feed> feeds = new ArrayList<>();
             Map<Long, FeedItem> lastItemsMap = new HashMap<>();
 
-            for (Feed feed: DBReader.getFeedList()) {
+            for (Feed feed : DBReader.getFeedList()) {
                 FeedPreferences prefs = feed.getPreferences();
                 if (prefs.getKeepUpdated() && prefs.getShowNotification()) {
                     List<FeedItem> outdatedFeedItems = DBReader.getFeedItemList(feed);
@@ -104,19 +94,18 @@ public class FeedUpdateWorker extends Worker {
         Resources res = context.getResources();
         String text = res.getQuantityString(R.plurals.new_episode_message, newEpisodes, newEpisodes, feed.getTitle());
 
-        /*
-        fixme
-        Intent intent = new Intent(context, OnlineFeedViewActivity.class);
+        Intent intent = new Intent("de.danoeh.antennapod.activity.MainActivity");
+
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra("fragment_feed_id", feed.getId());
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-         */
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_notification_bell_white)
                 .setContentTitle("New Episode")
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // fixme .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
         createNotificationChannel();
@@ -135,7 +124,7 @@ public class FeedUpdateWorker extends Worker {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
 
-            NotificationManager notificationManager =  context.getSystemService(NotificationManager.class);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
