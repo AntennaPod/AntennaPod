aa1c0ba7b4296940bd7d79dcb474b4269634ee65
Connectety-T <connectetyGit@yandex.com>
2020-10-11 00:12:30 +0200

    fixed not showing new episode notification.

--- core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
+++ core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
@@ -489,4 +489,27 @@ public class FeedItem extends FeedComponent implements ShownotesProvider, ImageR
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+
+        result = 31 * result + itemIdentifier.hashCode();
+        result = 31 * result + (int) (feedId ^ (feedId >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FeedItem other = (FeedItem) obj;
+        if (feedId != other.feedId)
+            return false;
+        return getIdentifyingValue().equals(other.getIdentifyingValue());
+    }
 }
