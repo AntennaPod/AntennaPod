c69a46d36c2e237c730c948274562663939659c8
DAMLA YILDIZ <damlayildiz19@ku.edu.tr>
2020-10-17 14:26:52 +0300

    create new channel for synchronization errors

--- core/src/main/java/de/danoeh/antennapod/core/sync/SyncService.java
+++ core/src/main/java/de/danoeh/antennapod/core/sync/SyncService.java
@@ -491,7 +491,7 @@ public class SyncService extends Worker {
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),
                 R.id.pending_intent_sync_error, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         Notification notification = new NotificationCompat.Builder(getApplicationContext(),
-                NotificationUtils.CHANNEL_ID_ERROR)
+                NotificationUtils.CHANNEL_ID_SYNC_ERROR)
                 .setContentTitle(getApplicationContext().getString(R.string.gpodnetsync_error_title))
                 .setContentText(description)
                 .setContentIntent(pendingIntent)
