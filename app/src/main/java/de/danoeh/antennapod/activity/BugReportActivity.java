75b7b41fa07da5d7f4b8a036ddb84bd846dda2a8
zawad hossain <32180355+zawad2221@users.noreply.github.com>
2020-10-24 13:32:22 +0600

    Share exported logs added (#4582)

--- app/src/main/java/de/danoeh/antennapod/activity/BugReportActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/BugReportActivity.java
@@ -3,11 +3,21 @@ package de.danoeh.antennapod.activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+
 import android.widget.TextView;
+
+
 import de.danoeh.antennapod.error.CrashReportWriter;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
@@ -18,6 +28,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * Displays the 'crash report' screen
@@ -63,12 +74,35 @@ public class BugReportActivity extends AppCompatActivity {
                 filename.createNewFile();
                 String cmd = "logcat -d -f " + filename.getAbsolutePath();
                 Runtime.getRuntime().exec(cmd);
-                Snackbar.make(findViewById(android.R.id.content),
-                        filename.getAbsolutePath(), Snackbar.LENGTH_SHORT).show();
+                //share file
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/*");
+                    String authString = getString(de.danoeh.antennapod.core.R.string.provider_authority);
+                    Uri fileUri = FileProvider.getUriForFile(this, authString, filename);
+                    i.putExtra(Intent.EXTRA_STREAM,  fileUri);
+                    i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                        PackageManager pm = getPackageManager();
+                        List<ResolveInfo> resInfos = pm.queryIntentActivities(i, PackageManager.MATCH_DEFAULT_ONLY);
+                        for (ResolveInfo resolveInfo : resInfos) {
+                            String packageName = resolveInfo.activityInfo.packageName;
+                            grantUriPermission(packageName, fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        }
+                    }
+                    String chooserTitle =  getString(de.danoeh.antennapod.core.R.string.share_file_label);
+                    startActivity(Intent.createChooser(i, chooserTitle));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    int strResId = R.string.log_file_share_exception;
+                    Snackbar.make(findViewById(android.R.id.content), strResId, Snackbar.LENGTH_LONG)
+                            .show();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
                 Snackbar.make(findViewById(android.R.id.content), e.getMessage(), Snackbar.LENGTH_LONG).show();
             }
         });
     }
+
 }
