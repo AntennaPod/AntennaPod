c972791ae5b010a4e968191ec91cb9e23e970e5d
Mohit Shah <mohitshah3111999@gmail.com>
2020-10-28 15:10:28 +0530

    Use Of ViewBinding in AddFeedFragment class

--- app/src/main/java/de/danoeh/antennapod/fragment/AddFeedFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/AddFeedFragment.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +22,8 @@ import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.MainActivity;
 import de.danoeh.antennapod.activity.OnlineFeedViewActivity;
 import de.danoeh.antennapod.activity.OpmlImportActivity;
+import de.danoeh.antennapod.databinding.AddfeedBinding;
+import de.danoeh.antennapod.databinding.EditTextDialogBinding;
 import de.danoeh.antennapod.discovery.CombinedSearcher;
 import de.danoeh.antennapod.discovery.FyydPodcastSearcher;
 import de.danoeh.antennapod.discovery.ItunesPodcastSearcher;
@@ -36,8 +37,10 @@ public class AddFeedFragment extends Fragment {
     public static final String TAG = "AddFeedFragment";
     private static final int REQUEST_CODE_CHOOSE_OPML_IMPORT_PATH = 1;
 
-    private EditText combinedFeedSearchBox;
     private MainActivity activity;
+
+    AddfeedBinding addFeedViewBinding;
+    EditTextDialogBinding editTextDialogViewBinding;
 
     @Override
     @Nullable
@@ -46,26 +49,29 @@ public class AddFeedFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View root = inflater.inflate(R.layout.addfeed, container, false);
-        activity = (MainActivity) getActivity();
-        ((AppCompatActivity) getActivity()).setSupportActionBar(root.findViewById(R.id.toolbar));
 
-        root.findViewById(R.id.btn_search_itunes).setOnClickListener(v
+        addFeedViewBinding = AddfeedBinding.bind(root);
+
+        activity = (MainActivity) getActivity();
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(addFeedViewBinding.toolbar);
+
+        addFeedViewBinding.searchItunesButton.setOnClickListener(v
                 -> activity.loadChildFragment(OnlineSearchFragment.newInstance(ItunesPodcastSearcher.class)));
-        root.findViewById(R.id.btn_search_fyyd).setOnClickListener(v
+        addFeedViewBinding.searchFyydButton.setOnClickListener(v
                 -> activity.loadChildFragment(OnlineSearchFragment.newInstance(FyydPodcastSearcher.class)));
-        root.findViewById(R.id.btn_search_gpodder).setOnClickListener(v
+        addFeedViewBinding.searchGpodderButton.setOnClickListener(v
                 -> activity.loadChildFragment(new GpodnetMainFragment()));
 
-        combinedFeedSearchBox = root.findViewById(R.id.combinedFeedSearchBox);
-        combinedFeedSearchBox.setOnEditorActionListener((v, actionId, event) -> {
+        addFeedViewBinding.podcastSearchButton.setOnEditorActionListener((v, actionId, event) -> {
             performSearch();
             return true;
         });
-        root.findViewById(R.id.btn_add_via_url).setOnClickListener(v
+
+        addFeedViewBinding.addViaUrlButton.setOnClickListener(v
                 -> showAddViaUrlDialog());
 
-        View butOpmlImport = root.findViewById(R.id.btn_opml_import);
-        butOpmlImport.setOnClickListener(v -> {
+        addFeedViewBinding.opmlImportButton.setOnClickListener(v -> {
             try {
                 Intent intentGetContentAction = new Intent(Intent.ACTION_GET_CONTENT);
                 intentGetContentAction.addCategory(Intent.CATEGORY_OPENABLE);
@@ -75,23 +81,26 @@ public class AddFeedFragment extends Fragment {
                 Log.e(TAG, "No activity found. Should never happen...");
             }
         });
-        root.findViewById(R.id.search_icon).setOnClickListener(view -> performSearch());
-        return root;
+        addFeedViewBinding.searchButton.setOnClickListener(view -> performSearch());
+        return addFeedViewBinding.getRoot();
     }
 
     private void showAddViaUrlDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle(R.string.add_podcast_by_url);
+
         View content = View.inflate(getContext(), R.layout.edit_text_dialog, null);
-        EditText editText = content.findViewById(R.id.text);
-        editText.setHint(R.string.add_podcast_by_url_hint);
+        editTextDialogViewBinding = EditTextDialogBinding.bind(content);
+
+        editTextDialogViewBinding.text.setHint(R.string.add_podcast_by_url_hint);
         ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
         String clipboardContent = clipboard.getText() != null ? clipboard.getText().toString() : "";
         if (clipboardContent.trim().startsWith("http")) {
-            editText.setText(clipboardContent.trim());
+            editTextDialogViewBinding.text.setText(clipboardContent.trim());
         }
-        builder.setView(content);
-        builder.setPositiveButton(R.string.confirm_label, (dialog, which) -> addUrl(editText.getText().toString()));
+
+        builder.setView(editTextDialogViewBinding.getRoot());
+        builder.setPositiveButton(R.string.confirm_label, (dialog, which) -> addUrl(editTextDialogViewBinding.text.getText().toString()));
         builder.setNegativeButton(R.string.cancel_label, null);
         builder.show();
     }
@@ -103,7 +112,7 @@ public class AddFeedFragment extends Fragment {
     }
 
     private void performSearch() {
-        String query = combinedFeedSearchBox.getText().toString();
+        String query = addFeedViewBinding.podcastSearchButton.getText().toString();
 
         if (query.matches("http[s]?://.*")) {
             addUrl(query);
