70024630793a999b4978552cf25af36a48ed9c14
Connectety-T <connectetyGit@yandex.com>
2020-11-04 23:40:54 +0100

    add braces to if

--- core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
+++ core/src/main/java/de/danoeh/antennapod/core/feed/FeedItem.java
@@ -499,15 +499,19 @@ public class FeedItem extends FeedComponent implements ShownotesProvider, ImageR
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         FeedItem other = (FeedItem) obj;
-        if (feedId != other.feedId)
+        if (feedId != other.feedId) {
             return false;
+        }
         return getIdentifyingValue().equals(other.getIdentifyingValue());
     }
 }
