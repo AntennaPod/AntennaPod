da2da916d77e0cb7d36c740688b3cc7d57eec7b2
Tony Tam <engineer.tonytam@pm.me>
2021-01-11 22:40:33 -0800

    newline

--- app/src/main/java/de/danoeh/antennapod/activity/WidgetConfigActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/WidgetConfigActivity.java
@@ -10,7 +10,6 @@ import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.appwidget.AppWidgetManager;
