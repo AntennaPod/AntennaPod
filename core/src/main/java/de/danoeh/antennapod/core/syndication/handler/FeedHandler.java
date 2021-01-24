5428b112f3ad29b763c5152b27ada2e21a62a25c
Tony Tam <engineer.tonytam@pm.me>
2021-01-23 21:01:13 -0800

    convert tabs to spaces

--- core/src/main/java/de/danoeh/antennapod/core/syndication/handler/FeedHandler.java
+++ core/src/main/java/de/danoeh/antennapod/core/syndication/handler/FeedHandler.java
@@ -16,22 +16,22 @@ import de.danoeh.antennapod.core.feed.Feed;
 
 public class FeedHandler {
 
-	public FeedHandlerResult parseFeed(Feed feed) throws SAXException, IOException,
-			ParserConfigurationException, UnsupportedFeedtypeException {
-		TypeGetter tg = new TypeGetter();
-		TypeGetter.Type type = tg.getType(feed);
-		SyndHandler handler = new SyndHandler(feed, type);
+    public FeedHandlerResult parseFeed(Feed feed) throws SAXException, IOException,
+            ParserConfigurationException, UnsupportedFeedtypeException {
+        TypeGetter tg = new TypeGetter();
+        TypeGetter.Type type = tg.getType(feed);
+        SyndHandler handler = new SyndHandler(feed, type);
 
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		factory.setNamespaceAware(true);
-		SAXParser saxParser = factory.newSAXParser();
-		File file = new File(feed.getFile_url());
-		Reader inputStreamReader = new XmlStreamReader(file);
-		InputSource inputSource = new InputSource(inputStreamReader);
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        SAXParser saxParser = factory.newSAXParser();
+        File file = new File(feed.getFile_url());
+        Reader inputStreamReader = new XmlStreamReader(file);
+        InputSource inputSource = new InputSource(inputStreamReader);
 
-		saxParser.parse(inputSource, handler);
-		inputStreamReader.close();
-		feed.dedupItems();
-		return new FeedHandlerResult(handler.state.feed, handler.state.alternateUrls);
-	}
+        saxParser.parse(inputSource, handler);
+        inputStreamReader.close();
+        feed.dedupItems();
+        return new FeedHandlerResult(handler.state.feed, handler.state.alternateUrls);
+    }
 }
