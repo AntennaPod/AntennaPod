c6b5d60a7c9cb8e297471d70593e5553bfb8fd9f
ByteHamster <info@bytehamster.com>
2021-02-06 12:46:36 +0100

    Don't set progress to 0 and then back to a number
    This triggers an animation

--- app/src/main/java/de/danoeh/antennapod/adapter/DownloadlistAdapter.java
+++ app/src/main/java/de/danoeh/antennapod/adapter/DownloadlistAdapter.java
@@ -68,8 +68,8 @@ public class DownloadlistAdapter extends BaseAdapter {
         holder.secondaryActionButton.setContentDescription(context.getString(R.string.cancel_download_label));
         holder.secondaryActionButton.setTag(downloader);
         holder.secondaryActionButton.setOnClickListener(butSecondaryListener);
-        holder.secondaryActionProgress.setPercentage(0, request);
 
+        boolean percentageWasSet = false;
         String status = "";
         if (request.getFeedfileType() == Feed.FEEDFILETYPE_FEED) {
             status += context.getString(R.string.download_type_feed);
@@ -85,7 +85,11 @@ public class DownloadlistAdapter extends BaseAdapter {
                 status += " / " + Formatter.formatShortFileSize(context, request.getSize());
                 holder.secondaryActionProgress.setPercentage(
                         0.01f * Math.max(1, request.getProgressPercent()), request);
+                percentageWasSet = true;
             }
+        }
+        if (!percentageWasSet) {
+            holder.secondaryActionProgress.setPercentage(0, request);
         }
         holder.status.setText(status);
 
