60968089ae4afe13ba48d64346ea39c542ad457b
Herbert Reiter <46045854+damoasda@users.noreply.github.com>
2021-02-07 17:57:09 +0100

    Refactoring: Remove ClientConfig.automaticDownloadAlgorithm (#4924)

--- app/src/main/java/de/danoeh/antennapod/config/ClientConfigurator.java
+++ app/src/main/java/de/danoeh/antennapod/config/ClientConfigurator.java
@@ -2,21 +2,20 @@ package de.danoeh.antennapod.config;
 
 import de.danoeh.antennapod.BuildConfig;
 import de.danoeh.antennapod.core.ClientConfig;
-import de.danoeh.antennapod.core.storage.APDownloadAlgorithm;
 
 /**
  * Configures the ClientConfig class of the core package.
  */
 class ClientConfigurator {
 
-    private ClientConfigurator(){}
+    private ClientConfigurator() {
+    }
 
     static {
         ClientConfig.USER_AGENT = "AntennaPod/" + BuildConfig.VERSION_NAME;
         ClientConfig.applicationCallbacks = new ApplicationCallbacksImpl();
         ClientConfig.downloadServiceCallbacks = new DownloadServiceCallbacksImpl();
         ClientConfig.playbackServiceCallbacks = new PlaybackServiceCallbacksImpl();
-        ClientConfig.automaticDownloadAlgorithm = new APDownloadAlgorithm();
         ClientConfig.castCallbacks = new CastCallbackImpl();
     }
 }
