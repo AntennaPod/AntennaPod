241935c601fd74fe0d2d969ed4b427ea3b270fed
Joschua Gandert <CreamyCookie@users.noreply.github.com>
2021-03-22 10:16:51 +0100

    Fixed certain file types not being recognized in local feeds (Closes #4802) (#5039)

--- core/src/main/java/de/danoeh/antennapod/core/feed/LocalFeedUpdater.java
+++ core/src/main/java/de/danoeh/antennapod/core/feed/LocalFeedUpdater.java
@@ -70,7 +70,21 @@ public class LocalFeedUpdater {
         Set<String> mediaFileNames = new HashSet<>();
         for (DocumentFile file : documentFolder.listFiles()) {
             String mime = file.getType();
-            if (mime != null && (mime.startsWith("audio/") || mime.startsWith("video/"))) {
+            if (mime == null) {
+                continue;
+            }
+
+            MediaType mediaType = MediaType.fromMimeType(mime);
+            if (mediaType == MediaType.UNKNOWN) {
+                String path = file.getUri().toString();
+                int fileExtensionPosition = path.lastIndexOf('.');
+                if (fileExtensionPosition >= 0) {
+                    String extensionWithoutDot = path.substring(fileExtensionPosition + 1);
+                    mediaType = MediaType.fromFileExtension(extensionWithoutDot);
+                }
+            }
+
+            if (mediaType == MediaType.AUDIO || mediaType == MediaType.VIDEO) {
                 mediaFiles.add(file);
                 mediaFileNames.add(file.getName());
             }
