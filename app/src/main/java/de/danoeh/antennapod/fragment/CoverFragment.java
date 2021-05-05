1c59643bf63d3548195532b2cbb05c017d90cb1f
ueen <ueli.sarnighausen@online.de>
2021-05-05 10:18:29 +0200

    fix

--- app/src/main/java/de/danoeh/antennapod/fragment/CoverFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/CoverFragment.java
@@ -142,11 +142,13 @@ public class CoverFragment extends Fragment {
         displayedChapterIndex = -1;
         refreshChapterData(ChapterUtils.getCurrentChapterIndex(media, media.getPosition()));
 
-        boolean hasShownotes = !StringUtils.isEmpty(media.getDescription());
-        int newVisibility = hasShownotes ? View.VISIBLE : View.INVISIBLE;
-        if (openDescriptionLayout.getVisibility() != newVisibility) {
-            openDescriptionLayout.setVisibility(hasShownotes ? View.VISIBLE : View.INVISIBLE);
-            ObjectAnimator.ofFloat(openDescriptionLayout, "alpha", hasShownotes ? 0 : 1, hasShownotes ? 1 : 0).setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime)).start();
+        if (media.getDescription() != null) {
+            boolean hasShownotes = !StringUtils.isEmpty(media.getDescription());
+            int newVisibility = hasShownotes ? View.VISIBLE : View.INVISIBLE;
+            if (openDescriptionLayout.getVisibility() != newVisibility) {
+                openDescriptionLayout.setVisibility(hasShownotes ? View.VISIBLE : View.INVISIBLE);
+                ObjectAnimator.ofFloat(openDescriptionLayout, "alpha", hasShownotes ? 0 : 1, hasShownotes ? 1 : 0).setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime)).start();
+            }
         }
     }
 
