2ecdbf2096a69992ad12504ce2da25dc0661908a
ueen <ueli.sarnighausen@online.de>
2021-05-29 18:20:33 +0200

    fix CI

--- app/src/main/java/de/danoeh/antennapod/fragment/EpisodesFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/EpisodesFragment.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.activity.MainActivity;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.util.download.AutoUpdateManager;
 import de.danoeh.antennapod.dialog.FilterDialog;
