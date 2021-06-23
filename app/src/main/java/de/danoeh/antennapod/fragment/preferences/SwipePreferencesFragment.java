c935556566468e462e1300f705690ae0d2a80a1f
ueen <ueli.sarnighausen@online.de>
2021-06-23 23:31:48 +0200

    added Queue to preferences

--- app/src/main/java/de/danoeh/antennapod/fragment/preferences/SwipePreferencesFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/preferences/SwipePreferencesFragment.java
@@ -6,9 +6,13 @@ import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.PreferenceActivity;
 import de.danoeh.antennapod.dialog.SwipeActionsDialog;
 import de.danoeh.antennapod.fragment.FeedItemlistFragment;
+import de.danoeh.antennapod.fragment.QueueFragment;
 
 public class SwipePreferencesFragment extends PreferenceFragmentCompat {
     private static final String PREF_SWIPE_FEED = "prefSwipeFeed";
+    private static final String PREF_SWIPE_QUEUE = "prefSwipeQueue";
+    //private static final String PREF_SWIPE_INBOX = "prefSwipeInbox";
+    //private static final String PREF_SWIPE_EPISODES = "prefSwipeEpisodes";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -16,6 +20,11 @@ public class SwipePreferencesFragment extends PreferenceFragmentCompat {
 
         findPreference(PREF_SWIPE_FEED).setOnPreferenceClickListener(preference -> {
             new SwipeActionsDialog(requireContext(), FeedItemlistFragment.TAG)
+                    .show(() -> { }, () -> { });
+            return true;
+        });
+        findPreference(PREF_SWIPE_QUEUE).setOnPreferenceClickListener(preference -> {
+            new SwipeActionsDialog(requireContext(), QueueFragment.TAG)
                     .show(() -> { }, () -> { });
             return true;
         });
