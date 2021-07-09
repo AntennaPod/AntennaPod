10b8cc3f4333af7ac3827eb5710adcbd715fd6df
ByteHamster <info@bytehamster.com>
2021-07-09 22:03:24 +0200

    Moved multi-select item to the bottom

--- app/src/main/java/de/danoeh/antennapod/adapter/QueueRecyclerAdapter.java
+++ app/src/main/java/de/danoeh/antennapod/adapter/QueueRecyclerAdapter.java
@@ -59,9 +59,9 @@ public class QueueRecyclerAdapter extends EpisodeItemListAdapter {
 
     @Override
     public void onCreateContextMenu(final ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getActivity().getMenuInflater();
         inflater.inflate(R.menu.queue_context, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
 
         if (!inActionMode()) {
             menu.findItem(R.id.multi_select).setVisible(true);
