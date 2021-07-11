94283caa6166be1c4e6c80d3d3ac5d790dab080a
ueen <ueli.sarnighausen@online.de>
2021-07-11 10:13:52 +0200

    fix CI style

--- app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/ShowFirstSwipeDialogAction.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/ShowFirstSwipeDialogAction.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import androidx.fragment.app.Fragment;
 
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.core.storage.DBWriter;
 import de.danoeh.antennapod.model.feed.FeedItem;
 import de.danoeh.antennapod.model.feed.FeedItemFilter;
 
