e23621ec6b3cda29aa4bdc86110e17a96d5a0320
ueen <ueli.sarnighausen@online.de>
2021-07-13 18:05:55 +0200

    EpisodeItem Cover to PodcastFeed

--- app/src/main/java/de/danoeh/antennapod/view/viewholder/EpisodeItemViewHolder.java
+++ app/src/main/java/de/danoeh/antennapod/view/viewholder/EpisodeItemViewHolder.java
@@ -1,5 +1,6 @@
 package de.danoeh.antennapod.view.viewholder;
 
+import android.content.Intent;
 import android.os.Build;
 import android.text.Layout;
 import android.text.format.Formatter;
@@ -132,6 +133,9 @@ public class EpisodeItemViewHolder extends RecyclerView.ViewHolder {
                     .withPlaceholderView(placeholder)
                     .withCoverView(cover)
                     .load();
+
+            Intent openFeed = MainActivity.getIntentToOpenFeed(activity, item.getFeedId());
+            cover.setOnClickListener(view -> activity.startActivity(openFeed));
         }
     }
 
