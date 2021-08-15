547605a78fb2b4e865f99d870e73dfc31815e013
sak96 <26397224+sak96@users.noreply.github.com>
2021-08-15 21:40:56 +0530

    Treat link without rel as rel=alternate
    src: https://datatracker.ietf.org/doc/html/rfc4287#section-4.2.7.2
    
    If the "rel" attribute is not present, the link element MUST
    be interpreted as if the link relation type is
    "alternate".

--- core/src/test/java/de/danoeh/antennapod/core/syndication/handler/AtomParserTest.java
+++ core/src/test/java/de/danoeh/antennapod/core/syndication/handler/AtomParserTest.java
@@ -14,6 +14,7 @@ import de.danoeh.antennapod.model.feed.FeedMedia;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Tests for Atom feeds in FeedHandler.
@@ -52,6 +53,34 @@ public class AtomParserTest {
             // chapters
             assertNull(item.getChapters());
         }
+    }
+
+    @Test
+    public void testEmptyRelLinks() throws Exception {
+        File feedFile = FeedParserTestHelper.getFeedFile("feed-atom-testEmptyRelLinks.xml");
+        Feed feed = FeedParserTestHelper.runFeedParser(feedFile);
+        assertEquals(Feed.TYPE_ATOM1, feed.getType());
+        assertEquals("title", feed.getTitle());
+        assertEquals("http://example.com/feed", feed.getFeedIdentifier());
+        assertEquals("http://example.com", feed.getLink());
+        assertEquals("This is the description", feed.getDescription());
+        assertNull(feed.getPaymentLinks());
+        assertEquals("http://example.com/picture", feed.getImageUrl());
+        assertEquals(1, feed.getItems().size());
+
+        // feed entry
+        FeedItem item = feed.getItems().get(0);
+        assertEquals("http://example.com/item-0", item.getItemIdentifier());
+        assertEquals("item-0", item.getTitle());
+        assertNull(item.getDescription());
+        assertEquals("http://example.com/items/0", item.getLink());
+        assertEquals(new Date(0), item.getPubDate());
+        assertNull(item.getPaymentLink());
+        assertEquals("http://example.com/picture", item.getImageLocation());
+        // media
+        assertFalse(item.hasMedia());
+        // chapters
+        assertNull(item.getChapters());
     }
 
     @Test
