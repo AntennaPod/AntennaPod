de8496f37f826fb20ab415b68e62eb298e18ef5d
ByteHamster <info@bytehamster.com>
2021-11-06 22:21:28 +0100

    Fix crash when deleting feed
    When the database is huge, loading the subscription item list takes
    a long time. Redirecting to the episodes screen after deleting
    sometimes results in the item list starting to update before. If the
    screen is then hidden before the Callable returns, the app crashes.

--- app/src/main/java/de/danoeh/antennapod/fragment/SubscriptionFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/SubscriptionFragment.java
@@ -351,7 +351,7 @@ public class SubscriptionFragment extends Fragment
             new RenameFeedDialog(getActivity(), feed).show();
             return true;
         } else if (itemId == R.id.remove_item) {
-            RemoveFeedDialog.show(getContext(), feed, null);
+            RemoveFeedDialog.show(getContext(), feed);
             return true;
         } else if (itemId == R.id.multi_select) {
             speedDialView.setVisibility(View.VISIBLE);
