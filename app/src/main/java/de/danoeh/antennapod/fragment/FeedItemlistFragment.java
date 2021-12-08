9d3d6e7428e6678b9b1dcb7fce9fdb56ffc7fda1
silansuslu <silan.suslu@hotmail.com>
2021-12-09 01:05:05 +0300

    Code changes for build

--- app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/FeedItemlistFragment.java
@@ -88,6 +88,7 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import de.danoeh.antennapod.view.OnKeyListenerForFragments;
 
 /**
  * Displays a list of FeedItems.
--- app/src/main/java/de/danoeh/antennapod/view/OnKeyListenerForFragments.java
+++ app/src/main/java/de/danoeh/antennapod/view/OnKeyListenerForFragments.java
@@ -5,3 +5,6 @@ import androidx.fragment.app.Fragment;
 public abstract class OnKeyListenerForFragments extends Fragment {
     public abstract void onKeyUp(int keyCode);
 }
+
+
+
