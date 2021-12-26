b7ee8a0c72e9dc9a746f68696b84d25bba230449
Tony Tam <engineer.tonytam@pm.me>
2021-12-25 23:11:55 -0800

    added preference for VPN on Wifi

--- core/src/main/java/de/danoeh/antennapod/core/util/NetworkUtils.java
+++ core/src/main/java/de/danoeh/antennapod/core/util/NetworkUtils.java
@@ -104,7 +104,8 @@ public class NetworkUtils {
             // TODO what if the wifi is also metered, maybe we should consider adding an option
             // to allow for VPN+Wifi always
             if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) &&
-                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) &&
+                    capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)) {
                 Log.d(TAG, "Network is VPN and WIFI, is this really not metered?");
                 return false;
             }
