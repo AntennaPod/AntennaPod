d94c4e867394af2284e648cf0fe97756ac72bf74
Tony Tam <engineer.tonytam@pm.me>
2022-01-02 00:03:06 -0800

    new column for upgrads KEY_PODCASTINDEX_CHAPTER_URL

--- core/src/main/java/de/danoeh/antennapod/core/storage/DBUpgrader.java
+++ core/src/main/java/de/danoeh/antennapod/core/storage/DBUpgrader.java
@@ -326,6 +326,11 @@ class DBUpgrader {
             db.execSQL("ALTER TABLE " + PodDBAdapter.TABLE_NAME_FEEDS
                     + " ADD COLUMN " + PodDBAdapter.KEY_MINIMAL_DURATION_FILTER + " INTEGER DEFAULT -1");
         }
+
+        if (oldVersion < 2090000) {
+            db.execSQL("ALTER TABLE " + PodDBAdapter.TABLE_NAME_FEED_ITEMS
+                    + " ADD COLUMN " + PodDBAdapter.KEY_PODCASTINDEX_CHAPTER_URL + " TEXT");
+        }
     }
 
 }
