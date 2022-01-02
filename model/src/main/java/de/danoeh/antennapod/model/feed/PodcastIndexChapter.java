f53d7a0b05947d9d5ce57ba2705007eb56fb626d
Tony Tam <engineer.tonytam@pm.me>
2022-01-02 00:02:14 -0800

    new type of chapter

--- model/src/main/java/de/danoeh/antennapod/model/feed/PodcastIndexChapter.java
+++ model/src/main/java/de/danoeh/antennapod/model/feed/PodcastIndexChapter.java
@@ -1,2 +1,29 @@
-package de.danoeh.antennapod.model.feed;public class PodcastIndexChapter {
+package de.danoeh.antennapod.model.feed;
+
+public class PodcastIndexChapter extends Chapter {
+    public static final int PODCASTINDEX_CHAPTER = 4;
+
+    public PodcastIndexChapter(int startTime,
+                               String title,
+                               String link,
+                               String imageUrl) {
+        super(startTime * 1000, title, link, imageUrl);
+    }
+
+    @Override
+    public int getChapterType() {
+        return PODCASTINDEX_CHAPTER;
+    }
+
+    @Override
+    public String toString() {
+        return "PodcastIndexChapter "
+                + getTitle()
+                + " "
+                + getStart()
+                + " "
+                + getLink()
+                + " "
+                + getImageUrl();
+    }
 }
