c8c0c765df757e70914e2bedb2068e2e16a0e033
Silverwarriorin <46795935+Silverwarriorin@users.noreply.github.com>
2022-02-11 12:10:57 -0500

    Cleaned Code

--- app/src/main/java/de/danoeh/antennapod/dialog/PlaybackControlsDialog.java
+++ app/src/main/java/de/danoeh/antennapod/dialog/PlaybackControlsDialog.java
@@ -1,45 +1,32 @@
 package de.danoeh.antennapod.dialog;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.TextView;
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.event.playback.SpeedChangedEvent;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
 import de.danoeh.antennapod.core.util.playback.PlaybackController;
-import de.danoeh.antennapod.view.PlaybackSpeedSeekBar;
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
-
 import java.util.List;
-import java.util.Locale;
-
 public class PlaybackControlsDialog extends DialogFragment {
     private PlaybackController controller;
     private AlertDialog dialog;
-    private PlaybackSpeedSeekBar speedSeekBar;
-    private TextView txtvPlaybackSpeed;
-
     public static PlaybackControlsDialog newInstance() {
         Bundle arguments = new Bundle();
         PlaybackControlsDialog dialog = new PlaybackControlsDialog();
         dialog.setArguments(arguments);
         return dialog;
     }
-
     public PlaybackControlsDialog() {
         // Empty constructor required for DialogFragment
     }
-
     @Override
     public void onStart() {
         super.onStart();
@@ -52,9 +39,7 @@ public class PlaybackControlsDialog extends DialogFragment {
         };
         controller.init();
         setupUi();
-        //EventBus.getDefault().register(this);
     }
-
     @Override
     public void onStop() {
         super.onStop();
@@ -62,7 +47,6 @@ public class PlaybackControlsDialog extends DialogFragment {
         controller = null;
         EventBus.getDefault().unregister(this);
     }
-
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -72,10 +56,7 @@ public class PlaybackControlsDialog extends DialogFragment {
                 .setPositiveButton(R.string.close_label, null).create();
         return dialog;
     }
-
     private void setupUi() {
-
-
         final CheckBox stereoToMono = dialog.findViewById(R.id.stereo_to_mono);
         stereoToMono.setChecked(UserPreferences.stereoToMono());
         if (controller != null && !controller.canDownmix()) {
@@ -83,7 +64,6 @@ public class PlaybackControlsDialog extends DialogFragment {
             String sonicOnly = getString(R.string.sonic_only);
             stereoToMono.setText(getString(R.string.stereo_to_mono) + " [" + sonicOnly + "]");
         }
-
         final CheckBox skipSilence = dialog.findViewById(R.id.skipSilence);
         skipSilence.setChecked(UserPreferences.isSkipSilence());
         if (!UserPreferences.useExoplayer()) {
@@ -102,10 +82,6 @@ public class PlaybackControlsDialog extends DialogFragment {
             }
         });
     }
-
-
-
-
     private void setupAudioTracks() {
         List<String> audioTracks = controller.getAudioTracks();
         int selectedAudioTrack = controller.getSelectedAudioTrack();
@@ -114,7 +90,6 @@ public class PlaybackControlsDialog extends DialogFragment {
             butAudioTracks.setVisibility(View.GONE);
             return;
         }
-
         butAudioTracks.setVisibility(View.VISIBLE);
         butAudioTracks.setText(audioTracks.get(selectedAudioTrack));
         butAudioTracks.setOnClickListener(v -> {
