dd1eb0aab66e5dbf3c77c019dc758d8dc3084c48
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2022-03-10 18:48:24 -0300

    finish activity if playback speed dialog is being dismissed by PlaybackSpeedDialogActivity

--- app/src/main/java/de/danoeh/antennapod/dialog/VariableSpeedDialog.java
+++ app/src/main/java/de/danoeh/antennapod/dialog/VariableSpeedDialog.java
@@ -1,5 +1,6 @@
 package de.danoeh.antennapod.dialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,6 +15,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.snackbar.Snackbar;
 import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.PlaybackSpeedDialogActivity;
 import de.danoeh.antennapod.event.playback.SpeedChangedEvent;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
 import de.danoeh.antennapod.core.util.playback.PlaybackController;
@@ -64,6 +66,14 @@ public class VariableSpeedDialog extends BottomSheetDialogFragment {
         controller.release();
         controller = null;
         EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        if (getActivity().getClass().getName().equals(PlaybackSpeedDialogActivity.class.getName())) {
+            getActivity().finish();
+        }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
