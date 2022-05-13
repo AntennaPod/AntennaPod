4fbfd3febf95b8075de4a1e811a29f1ef45ee0b6
adrns <adrns@@users.noreply.github.com>
2022-05-13 10:44:10 +0200

    Add 'open podcast' button to feed statistics modal

--- ui/app-start-intent/src/main/java/de/danoeh/antennapod/ui/appstartintent/MainActivityStarter.java
+++ ui/app-start-intent/src/main/java/de/danoeh/antennapod/ui/appstartintent/MainActivityStarter.java
@@ -12,6 +12,8 @@ import android.os.Build;
 public class MainActivityStarter {
     public static final String INTENT = "de.danoeh.antennapod.intents.MAIN_ACTIVITY";
     public static final String EXTRA_OPEN_PLAYER = "open_player";
+    public static final String EXTRA_FEED_ID = "fragment_feed_id";
+    public static final String EXTRA_STARTED_FROM_SEARCH = "started_from_search";
 
     private final Intent intent;
     private final Context context;
@@ -37,6 +39,12 @@ public class MainActivityStarter {
 
     public MainActivityStarter withOpenPlayer() {
         intent.putExtra(EXTRA_OPEN_PLAYER, true);
+        return this;
+    }
+
+    public MainActivityStarter withOpenFeed(long feedId) {
+        intent.putExtra(EXTRA_FEED_ID, feedId);
+        intent.putExtra(EXTRA_STARTED_FROM_SEARCH, true);
         return this;
     }
 }
