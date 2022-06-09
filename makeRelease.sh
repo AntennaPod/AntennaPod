0c38218141fe1b164016a491f6f17592df5d18a5
ByteHamster <info@bytehamster.com>
2022-06-09 22:21:57 +0200

    Link to localized website version from about screen

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage:\n  ./makeRelease 1.2.3rc0 Directory/"
-    exit 1
-fi
-
-set -e
-DEST=$2
-VERSION_NAME=$1
-
-rm CONTRIBUTORS
-./createDevelopersList.py
-./createTranslatorsList.py
-
-./gradlew clean assemblePlayDebug assemblePlayRelease
-cp app/build/outputs/apk/app-play-debug.apk $DEST/AntennaPod_${VERSION_NAME}_debug.apk
-cp app/build/outputs/apk/app-play-release.apk $DEST/AntennaPod_${VERSION_NAME}_release.apk
+curl -s https://raw.githubusercontent.com/AntennaPod/antennapod.github.io/master/_config.yml | yq -r ".languages[]" > ./app/src/main/assets/website-languages.txt
+python ./createContributors.py
