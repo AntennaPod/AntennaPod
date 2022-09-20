7b1fb5766cb232a708116b4263ba797e18e6c8a8
Erik Johnson <palehose@gmail.com>
2022-09-20 07:04:24 -0500

    Use FeedItem.NEW

--- app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/TogglePlaybackStateSwipeAction.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/TogglePlaybackStateSwipeAction.java
@@ -39,7 +39,7 @@ public class TogglePlaybackStateSwipeAction implements SwipeAction {
 
     @Override
     public boolean willRemove(FeedItemFilter filter, FeedItem item) {
-        if (item.getPlayState() == item.NEW) {
+        if (item.getPlayState() == FeedItem.NEW) {
             return filter.showPlayed || filter.showNew;
         } else {
             return filter.showUnplayed || filter.showPlayed || filter.showNew;
