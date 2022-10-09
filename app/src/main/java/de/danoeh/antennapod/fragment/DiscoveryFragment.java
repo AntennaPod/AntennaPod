cf1c72cb1d2df2f707f9514da358d76e9f4034e9
Pinkolik <mrpinkolik@gmail.com>
2022-10-09 22:50:39 +0500

    issue-6098 fix code-style

--- app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryFragment.java
@@ -3,7 +3,6 @@ package de.danoeh.antennapod.fragment;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
