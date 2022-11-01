f65df32ca4dc87671f3aea63c3972a46c039e579
VishnuSanal <t.v.s10123@gmail.com>
2022-11-01 22:16:44 +0530

    add confirmation dialog for clearing history

--- app/src/main/java/de/danoeh/antennapod/fragment/PlaybackHistoryFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/PlaybackHistoryFragment.java
@@ -1,5 +1,6 @@
 package de.danoeh.antennapod.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.core.dialog.ConfirmationDialog;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.DBWriter;
 import de.danoeh.antennapod.event.playback.PlaybackHistoryEvent;
@@ -55,7 +57,20 @@ public class PlaybackHistoryFragment extends EpisodesListFragment {
             return true;
         }
         if (item.getItemId() == R.id.clear_history_item) {
-            DBWriter.clearPlaybackHistory();
+
+            ConfirmationDialog conDialog = new ConfirmationDialog(
+                    getActivity(),
+                    de.danoeh.antennapod.ui.statistics.R.string.clear_playback_history,
+                    de.danoeh.antennapod.ui.statistics.R.string.clear_playback_history_msg) {
+
+                @Override
+                public void onConfirmButtonPressed(DialogInterface dialog) {
+                    dialog.dismiss();
+                    DBWriter.clearPlaybackHistory();
+                }
+            };
+            conDialog.createNewDialog().show();
+
             return true;
         }
         return false;
