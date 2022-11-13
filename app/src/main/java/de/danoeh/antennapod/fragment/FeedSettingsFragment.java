10ae6a82c11bbaed48c181c54fb5054d9356479a
Ricardo Borges Jr <ricardoborges.jr@gmail.com>
2022-11-13 13:13:33 -0300

    Undo feed URL edit

--- app/src/main/java/de/danoeh/antennapod/fragment/FeedSettingsFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/FeedSettingsFragment.java
@@ -16,7 +16,6 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import de.danoeh.antennapod.R;
-import de.danoeh.antennapod.dialog.EditUrlSettingsDialog;
 import de.danoeh.antennapod.event.settings.SkipIntroEndingChangedEvent;
 import de.danoeh.antennapod.event.settings.SpeedPresetChangedEvent;
 import de.danoeh.antennapod.event.settings.VolumeAdaptionChangedEvent;
@@ -160,7 +159,6 @@ public class FeedSettingsFragment extends Fragment {
                         setupFeedAutoSkipPreference();
                         setupEpisodeNotificationPreference();
                         setupTags();
-                        setupFeedEditUrlPreference();
 
                         updateAutoDeleteSummary();
                         updateVolumeReductionValue();
@@ -198,20 +196,6 @@ public class FeedSettingsFragment extends Fragment {
                                 new SkipIntroEndingChangedEvent(feedPreferences.getFeedSkipIntro(),
                                         feedPreferences.getFeedSkipEnding(),
                                         feed.getId()));
-                    }
-                }.show();
-                return false;
-            });
-        }
-
-        private void setupFeedEditUrlPreference() {
-            findPreference(PREF_EDIT_FEED_URL).setOnPreferenceClickListener(preference -> {
-                new EditUrlSettingsDialog(getContext(),
-                        feed) {
-                    @Override
-                    protected void onConfirmed(String origial, String updated) {
-                        DBWriter.updateFeedDownloadURL(origial, updated);
-                        feed.setDownload_url(updated);
                     }
                 }.show();
                 return false;
