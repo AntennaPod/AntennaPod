d6c4e3dabb8ef1eeca03efc1867a69fb506df8c1
VishnuSanal <t.v.s10123@gmail.com>
2022-12-27 11:53:21 +0530

    checkstyle: add braces to if statement

--- app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/RemoveFromHistorySwipeAction.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/swipeactions/RemoveFromHistorySwipeAction.java
@@ -50,12 +50,13 @@ public class RemoveFromHistorySwipeAction implements SwipeAction {
                             if(DBWriter.addItemToPlaybackHistory(
                                     item.getMedia(),
                                     item.getMedia().getPlaybackCompletionDate()
-                            ).isDone())
+                            ).isDone()) {
                                 ((MainActivity) fragment.requireActivity())
                                         .showSnackbarAbovePlayer(
                                                 R.string.restored_history_label,
                                                 Snackbar.LENGTH_LONG
                                         );
+                            }
                         }
                 );
     }
