831593aa93074a4fa4010a72b97194307482956f
ByteHamster <info@bytehamster.com>
2023-01-29 13:48:43 +0100

    Don't stop service between episodes

--- core/src/main/java/de/danoeh/antennapod/core/service/playback/PlaybackServiceStateManager.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/playback/PlaybackServiceStateManager.java
@@ -1,10 +1,12 @@
 package de.danoeh.antennapod.core.service.playback;
 
 import android.app.Notification;
+import android.util.Log;
 
 import androidx.core.app.ServiceCompat;
 
 class PlaybackServiceStateManager {
+    private static final String TAG = "PlaybackSrvState";
     private final PlaybackService playbackService;
 
     private volatile boolean isInForeground = false;
@@ -15,17 +17,20 @@ class PlaybackServiceStateManager {
     }
 
     void startForeground(int notificationId, Notification notification) {
+        Log.d(TAG, "startForeground");
         playbackService.startForeground(notificationId, notification);
         isInForeground = true;
     }
 
     void stopService() {
+        Log.d(TAG, "stopService");
         stopForeground(true);
         playbackService.stopSelf();
         hasReceivedValidStartCommand = false;
     }
 
     void stopForeground(boolean removeNotification) {
+        Log.d(TAG, "stopForeground");
         if (isInForeground) {
             if (removeNotification) {
                 ServiceCompat.stopForeground(playbackService, ServiceCompat.STOP_FOREGROUND_REMOVE);
