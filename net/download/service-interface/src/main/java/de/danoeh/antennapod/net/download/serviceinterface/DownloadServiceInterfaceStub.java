0bc86c0ca8433da97f58f9b9683222860b94df89
ByteHamster <info@bytehamster.com>
2023-02-19 13:18:43 +0100

    Add retry button to download failed notifications

--- net/download/service-interface/src/main/java/de/danoeh/antennapod/net/download/serviceinterface/DownloadServiceInterfaceStub.java
+++ net/download/service-interface/src/main/java/de/danoeh/antennapod/net/download/serviceinterface/DownloadServiceInterfaceStub.java
@@ -1,10 +1,15 @@
 package de.danoeh.antennapod.net.download.serviceinterface;
 
 import android.content.Context;
+import android.content.Intent;
 
 public class DownloadServiceInterfaceStub extends DownloadServiceInterface {
 
     public void download(Context context, boolean cleanupMedia, DownloadRequest... requests) {
+    }
+
+    public Intent makeDownloadIntent(Context context, boolean cleanupMedia, DownloadRequest... requests) {
+        return null;
     }
 
     public void refreshAllFeeds(Context context, boolean initiatedByUser) {
