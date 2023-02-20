e58e2d063948fe52e5fed1536548b6c4220be8fb
ByteHamster <ByteHamster@users.noreply.github.com>
2023-02-20 23:16:18 +0100

    Force-refresh feeds that failed before (#6332)
    Apparently some servers return "Not Modified", even though a broken feed
    was fixed in the meantime. When refreshing all feeds, now force-refresh
    the feeds that previously failed.

--- core/src/main/java/de/danoeh/antennapod/core/service/download/DownloadService.java
+++ core/src/main/java/de/danoeh/antennapod/core/service/download/DownloadService.java
@@ -468,6 +468,9 @@ public class DownloadService extends Service {
             if (feed.getPreferences().getKeepUpdated()) {
                 DownloadRequest.Builder builder = DownloadRequestCreator.create(feed);
                 builder.withInitiatedByUser(initiatedByUser);
+                if (feed.hasLastUpdateFailed()) {
+                    builder.setForce(true);
+                }
                 addNewRequest(builder.build());
             }
         }
