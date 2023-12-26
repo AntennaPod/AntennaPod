ee9d5e24b1e8244e04d48a39ee5b7e231a2cbd0c
ueen <ueli.sarnighausen@online.de>
2023-12-26 16:43:13 +0100

    fix unused imports

--- app/src/main/java/de/danoeh/antennapod/dialog/ItemFilterDialog.java
+++ app/src/main/java/de/danoeh/antennapod/dialog/ItemFilterDialog.java
@@ -3,7 +3,6 @@ package de.danoeh.antennapod.dialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
