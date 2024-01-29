2e71bfa12507955636071d4193fc9a6d7e8ca9c3
Tony Tam <engineer.tonytam@gmail.com>
2024-01-28 18:41:45 -0800

    sync

--- model/src/main/java/de/danoeh/antennapod/model/feed/FeedFile.java
+++ model/src/main/java/de/danoeh/antennapod/model/feed/FeedFile.java
@@ -78,7 +78,6 @@ public abstract class FeedFile extends FeedComponent {
         return file_url;
     }
 
-
     /**
      * Changes the file_url of this FeedFile. Setting this value to
      * null will also set the downloaded-attribute to false.
