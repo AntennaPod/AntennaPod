1edceb1b977d9dbffe916ecc187edd67870e55ab
Tony Tam <149837+tonytamsf@users.noreply.github.com>
2024-02-05 04:42:59 +0800

    Transcript semantic parsing (#6852)

---
+++ model/src/main/java/de/danoeh/antennapod/model/feed/Transcript.java
@@ -0,0 +1,28 @@
+package de.danoeh.antennapod.model.feed;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Transcript {
+
+    private final TreeMap<Long, TranscriptSegment> segmentsMap = new TreeMap<>();
+
+    public void addSegment(TranscriptSegment segment) {
+        segmentsMap.put(segment.getStartTime(), segment);
+    }
+
+    public TranscriptSegment getSegmentAtTime(long time) {
+        if (segmentsMap.floorEntry(time) == null) {
+            return null;
+        }
+        return segmentsMap.floorEntry(time).getValue();
+    }
+
+    public int getSegmentCount() {
+        return segmentsMap.size();
+    }
+
+    public Map.Entry<Long, TranscriptSegment> getEntryAfterTime(long time) {
+        return segmentsMap.ceilingEntry(time);
+    }
+}
