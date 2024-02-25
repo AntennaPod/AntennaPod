82c93bf7ee3d91395533068fdfe640dfa53113fe
ByteHamster <ByteHamster@users.noreply.github.com>
2024-02-25 14:01:03 +0100

    Guess next episode release date (#6925)

--- ui/statistics/src/main/java/de/danoeh/antennapod/ui/statistics/feed/FeedStatisticsFragment.java
+++ ui/statistics/src/main/java/de/danoeh/antennapod/ui/statistics/feed/FeedStatisticsFragment.java
@@ -2,6 +2,7 @@ package de.danoeh.antennapod.ui.statistics.feed;
 
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,23 @@ import androidx.fragment.app.Fragment;
 import de.danoeh.antennapod.core.storage.DBReader;
 import de.danoeh.antennapod.core.storage.StatisticsItem;
 import de.danoeh.antennapod.core.util.Converter;
+import de.danoeh.antennapod.core.util.DateFormatter;
+import de.danoeh.antennapod.core.util.ReleaseScheduleGuesser;
+import de.danoeh.antennapod.model.feed.FeedItem;
+import de.danoeh.antennapod.model.feed.FeedItemFilter;
+import de.danoeh.antennapod.model.feed.SortOrder;
+import de.danoeh.antennapod.ui.statistics.R;
 import de.danoeh.antennapod.ui.statistics.databinding.FeedStatisticsBinding;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class FeedStatisticsFragment extends Fragment {
@@ -66,7 +77,17 @@ public class FeedStatisticsFragment extends Fragment {
 
                     for (StatisticsItem statisticsItem : statisticsData.feedTime) {
                         if (statisticsItem.feed.getId() == feedId) {
-                            return statisticsItem;
+                            List<FeedItem> items = DBReader.getFeedItemList(statisticsItem.feed,
+                                    FeedItemFilter.unfiltered(), SortOrder.DATE_OLD_NEW);
+                            List<Date> dates = new ArrayList<>();
+                            for (FeedItem item : items) {
+                                dates.add(item.getPubDate());
+                            }
+                            ReleaseScheduleGuesser.Guess guess = null;
+                            if (dates.size() > 1) {
+                                guess = ReleaseScheduleGuesser.performGuess(dates);
+                            }
+                            return new Pair<>(statisticsItem, guess);
                         }
                     }
                     return null;
@@ -76,13 +97,80 @@ public class FeedStatisticsFragment extends Fragment {
                         .subscribe(this::showStats, Throwable::printStackTrace);
     }
 
-    private void showStats(StatisticsItem s) {
+    private String getReadableDay(int day) {
+        switch (day) {
+            case Calendar.MONDAY:
+                return getString(R.string.release_schedule_monday);
+            case Calendar.TUESDAY:
+                return getString(R.string.release_schedule_tuesday);
+            case Calendar.WEDNESDAY:
+                return getString(R.string.release_schedule_wednesday);
+            case Calendar.THURSDAY:
+                return getString(R.string.release_schedule_thursday);
+            case Calendar.FRIDAY:
+                return getString(R.string.release_schedule_friday);
+            case Calendar.SATURDAY:
+                return getString(R.string.release_schedule_saturday);
+            case Calendar.SUNDAY:
+                return getString(R.string.release_schedule_sunday);
+            default:
+                return "error";
+        }
+    }
+
+    private String getReadableSchedule(ReleaseScheduleGuesser.Guess guess) {
+        switch (guess.schedule) {
+            case DAILY:
+                return getString(R.string.release_schedule_daily);
+            case WEEKDAYS:
+                return getString(R.string.release_schedule_weekdays);
+            case WEEKLY:
+                return getString(R.string.release_schedule_weekly) + ", " + getReadableDay(guess.days.get(0));
+            case BIWEEKLY:
+                return getString(R.string.release_schedule_biweekly) + ", " + getReadableDay(guess.days.get(0));
+            case MONTHLY:
+                return getString(R.string.release_schedule_monthly);
+            case FOURWEEKLY:
+                return getString(R.string.release_schedule_monthly) + ", " + getReadableDay(guess.days.get(0));
+            case SPECIFIC_DAYS:
+                StringBuilder days = new StringBuilder();
+                for (int i = 0; i < guess.days.size(); i++) {
+                    if (i != 0) {
+                        days.append(", ");
+                    }
+                    days.append(getReadableDay(guess.days.get(i)));
+                }
+                return days.toString();
+            default:
+                return getString(R.string.statistics_expected_next_episode_unknown);
+        }
+    }
+
+    private void showStats(Pair<StatisticsItem, ReleaseScheduleGuesser.Guess> p) {
+        StatisticsItem s = p.first;
         viewBinding.startedTotalLabel.setText(String.format(Locale.getDefault(), "%d / %d",
                 s.episodesStarted, s.episodes));
         viewBinding.timePlayedLabel.setText(Converter.shortLocalizedDuration(getContext(), s.timePlayed));
         viewBinding.totalDurationLabel.setText(Converter.shortLocalizedDuration(getContext(), s.time));
         viewBinding.onDeviceLabel.setText(String.format(Locale.getDefault(), "%d", s.episodesDownloadCount));
         viewBinding.spaceUsedLabel.setText(Formatter.formatShortFileSize(getContext(), s.totalDownloadSize));
+
+        ReleaseScheduleGuesser.Guess guess = p.second;
+        if (!s.feed.getPreferences().getKeepUpdated()) {
+            viewBinding.expectedNextEpisodeLabel.setText(R.string.updates_disabled_label);
+        } else if (guess == null || guess.nextExpectedDate.getTime() <= new Date().getTime() - 7 * 24 * 3600000L) {
+            // More than 30 days delayed
+            viewBinding.expectedNextEpisodeLabel.setText(R.string.statistics_expected_next_episode_unknown);
+        } else {
+            String text = DateFormatter.formatAbbrev(getContext(), guess.nextExpectedDate);
+            if (guess.nextExpectedDate.getTime() <= new Date().getTime()) {
+                text = getString(R.string.statistics_expected_next_episode_any_day);
+            }
+            if (guess.schedule != ReleaseScheduleGuesser.Schedule.UNKNOWN) {
+                text += " (" + getReadableSchedule(guess) + ")";
+            }
+            viewBinding.expectedNextEpisodeLabel.setText(text);
+        }
     }
 
     @Override
