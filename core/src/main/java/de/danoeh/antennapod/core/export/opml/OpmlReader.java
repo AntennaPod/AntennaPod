44e123105c3f7ac24ee043c53cd086bf4a7c91d0
ByteHamster <info@bytehamster.com>
2024-03-04 22:39:09 +0100

    Backport: Remove tab characters from last 6 files

--- core/src/main/java/de/danoeh/antennapod/core/export/opml/OpmlReader.java
+++ core/src/main/java/de/danoeh/antennapod/core/export/opml/OpmlReader.java
@@ -13,78 +13,81 @@ import java.util.ArrayList;
 
 import de.danoeh.antennapod.core.BuildConfig;
 
-/** Reads OPML documents. */
+/**
+ * Reads OPML documents.
+ */
 public class OpmlReader {
-	private static final String TAG = "OpmlReader";
-	
-	// ATTRIBUTES
-	private boolean isInOpml = false;
-	private ArrayList<OpmlElement> elementList;
+    private static final String TAG = "OpmlReader";
 
-	/**
-	 * Reads an Opml document and returns a list of all OPML elements it can
-	 * find
-	 * 
-	 * @throws IOException
-	 * @throws XmlPullParserException
-	 */
-	public ArrayList<OpmlElement> readDocument(Reader reader)
-			throws XmlPullParserException, IOException {
-		elementList = new ArrayList<>();
-		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		factory.setNamespaceAware(true);
-		XmlPullParser xpp = factory.newPullParser();
-		xpp.setInput(reader);
-		int eventType = xpp.getEventType();
+    private boolean isInOpml = false;
 
-		while (eventType != XmlPullParser.END_DOCUMENT) {
-			switch (eventType) {
-			case XmlPullParser.START_DOCUMENT:
-				if (BuildConfig.DEBUG)
-					Log.d(TAG, "Reached beginning of document");
-				break;
-			case XmlPullParser.START_TAG:
-				if (xpp.getName().equals(OpmlSymbols.OPML)) {
-					isInOpml = true;
-					if (BuildConfig.DEBUG)
-						Log.d(TAG, "Reached beginning of OPML tree.");
-				} else if (isInOpml && xpp.getName().equals(OpmlSymbols.OUTLINE)) {
-					if (BuildConfig.DEBUG)
-						Log.d(TAG, "Found new Opml element");
-					OpmlElement element = new OpmlElement();
-					
-					final String title = xpp.getAttributeValue(null, OpmlSymbols.TITLE);
-					if (!TextUtils.isEmpty(title)) {
-						Log.i(TAG, "Using title: " + title);
-						element.setText(title);
-					} else {
-						Log.i(TAG, "Title not found, using text");
-						element.setText(xpp.getAttributeValue(null, OpmlSymbols.TEXT));			
-					}
-					element.setXmlUrl(xpp.getAttributeValue(null, OpmlSymbols.XMLURL));
-					element.setHtmlUrl(xpp.getAttributeValue(null, OpmlSymbols.HTMLURL));
-					element.setType(xpp.getAttributeValue(null, OpmlSymbols.TYPE));
-					if (!TextUtils.isEmpty(element.getXmlUrl())) {
-						if (!TextUtils.isEmpty(element.getText())) {
-							Log.i(TAG, "Opml element has no text attribute.");
-							element.setText(element.getXmlUrl());
-						}
-						elementList.add(element);
-					} else {
-						if (BuildConfig.DEBUG)
-							Log.d(TAG,
-									"Skipping element because of missing xml url");
-					}
-				}
-				break;
-			}
-			eventType = xpp.next();
-		}
+    /**
+     * Reads an Opml document and returns a list of all OPML elements it can
+     * find
+     */
+    public ArrayList<OpmlElement> readDocument(Reader reader)
+            throws XmlPullParserException, IOException {
+        ArrayList<OpmlElement> elementList = new ArrayList<>();
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xpp = factory.newPullParser();
+        xpp.setInput(reader);
+        int eventType = xpp.getEventType();
 
-		if (BuildConfig.DEBUG)
-			Log.d(TAG, "Parsing finished.");
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            switch (eventType) {
+                case XmlPullParser.START_DOCUMENT:
+                    if (BuildConfig.DEBUG) {
+                        Log.d(TAG, "Reached beginning of document");
+                    }
+                    break;
+                case XmlPullParser.START_TAG:
+                    if (xpp.getName().equals(OpmlSymbols.OPML)) {
+                        isInOpml = true;
+                        if (BuildConfig.DEBUG) {
+                            Log.d(TAG, "Reached beginning of OPML tree.");
+                        }
+                    } else if (isInOpml && xpp.getName().equals(OpmlSymbols.OUTLINE)) {
+                        if (BuildConfig.DEBUG) {
+                            Log.d(TAG, "Found new Opml element");
+                        }
+                        OpmlElement element = new OpmlElement();
 
-		return elementList;
-	}
+                        final String title = xpp.getAttributeValue(null, OpmlSymbols.TITLE);
+                        if (!TextUtils.isEmpty(title)) {
+                            Log.i(TAG, "Using title: " + title);
+                            element.setText(title);
+                        } else {
+                            Log.i(TAG, "Title not found, using text");
+                            element.setText(xpp.getAttributeValue(null, OpmlSymbols.TEXT));
+                        }
+                        element.setXmlUrl(xpp.getAttributeValue(null, OpmlSymbols.XMLURL));
+                        element.setHtmlUrl(xpp.getAttributeValue(null, OpmlSymbols.HTMLURL));
+                        element.setType(xpp.getAttributeValue(null, OpmlSymbols.TYPE));
+                        if (!TextUtils.isEmpty(element.getXmlUrl())) {
+                            if (!TextUtils.isEmpty(element.getText())) {
+                                Log.i(TAG, "Opml element has no text attribute.");
+                                element.setText(element.getXmlUrl());
+                            }
+                            elementList.add(element);
+                        } else {
+                            if (BuildConfig.DEBUG) {
+                                Log.d(TAG, "Skipping element because of missing xml url");
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            eventType = xpp.next();
+        }
+
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Parsing finished.");
+        }
+
+        return elementList;
+    }
 
 }
--- core/src/main/java/de/danoeh/antennapod/core/util/comparator/PlaybackCompletionDateComparator.java
+++ core/src/main/java/de/danoeh/antennapod/core/util/comparator/PlaybackCompletionDateComparator.java
@@ -5,15 +5,15 @@ import java.util.Comparator;
 import de.danoeh.antennapod.model.feed.FeedItem;
 
 public class PlaybackCompletionDateComparator implements Comparator<FeedItem> {
-	
-	public int compare(FeedItem lhs, FeedItem rhs) {
-		if (lhs.getMedia() != null
-				&& lhs.getMedia().getPlaybackCompletionDate() != null
-				&& rhs.getMedia() != null
-				&& rhs.getMedia().getPlaybackCompletionDate() != null) {
-			return rhs.getMedia().getPlaybackCompletionDate()
-					.compareTo(lhs.getMedia().getPlaybackCompletionDate());
-		}
-		return 0;
-	}
+
+    public int compare(FeedItem lhs, FeedItem rhs) {
+        if (lhs.getMedia() != null
+                && lhs.getMedia().getPlaybackCompletionDate() != null
+                && rhs.getMedia() != null
+                && rhs.getMedia().getPlaybackCompletionDate() != null) {
+            return rhs.getMedia().getPlaybackCompletionDate()
+                    .compareTo(lhs.getMedia().getPlaybackCompletionDate());
+        }
+        return 0;
+    }
 }
