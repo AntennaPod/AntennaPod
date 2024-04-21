df43455c7d03475b762c61d6fdb186e2dbae794f
flofriday <flohacksfriday@gmail.com>
2024-04-21 22:48:07 +0200

    Fix formatting from merge

--- ui/preferences/src/main/java/de/danoeh/antennapod/ui/preferences/screen/about/SimpleIconListAdapter.java
+++ ui/preferences/src/main/java/de/danoeh/antennapod/ui/preferences/screen/about/SimpleIconListAdapter.java
@@ -8,11 +8,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.FitCenter;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+
 import de.danoeh.antennapod.ui.preferences.R;
 
 import java.util.List;
@@ -48,7 +50,7 @@ public class SimpleIconListAdapter<T extends SimpleIconListAdapter.ListItem> ext
                     .apply(new RequestOptions()
                             .diskCacheStrategy(DiskCacheStrategy.NONE)
                             .transform(new FitCenter(), new RoundedCorners((int)
-                                (4 * context.getResources().getDisplayMetrics().density)))
+                                    (4 * context.getResources().getDisplayMetrics().density)))
                             .dontAnimate())
                     .into(((ImageView) view.findViewById(R.id.icon)));
         }
