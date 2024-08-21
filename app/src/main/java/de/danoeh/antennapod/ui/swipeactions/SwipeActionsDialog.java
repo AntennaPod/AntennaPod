b79a04756992c2d37fa6759dd649d8db61339151
ccell <70972592+Mudit-pip@users.noreply.github.com>
2024-08-21 23:49:00 +0530

    Removed unused import in SwipeActionsDialog.java

--- app/src/main/java/de/danoeh/antennapod/ui/swipeactions/SwipeActionsDialog.java
+++ app/src/main/java/de/danoeh/antennapod/ui/swipeactions/SwipeActionsDialog.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ScrollView;
 
 import androidx.appcompat.app.AlertDialog;
