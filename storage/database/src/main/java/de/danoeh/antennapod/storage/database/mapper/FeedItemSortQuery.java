638c6af729c43017ecf23af75fef633a191475c2
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-10-17 20:48:57 +0530

    update `KEY_FILE_URL` with `KEY_LINK` as the `sortOrder`

--- storage/database/src/main/java/de/danoeh/antennapod/storage/database/mapper/FeedItemSortQuery.java
+++ storage/database/src/main/java/de/danoeh/antennapod/storage/database/mapper/FeedItemSortQuery.java
@@ -27,9 +27,9 @@ public class FeedItemSortQuery {
             case DATE_OLD_NEW:
                 return PodDBAdapter.TABLE_NAME_FEED_ITEMS + "." + PodDBAdapter.KEY_PUBDATE + " " + "ASC";
             case EPISODE_FILENAME_A_Z:
-                return PodDBAdapter.KEY_FILE_URL + " " + "ASC";
+                return PodDBAdapter.KEY_LINK + " " + "ASC";
             case EPISODE_FILENAME_Z_A:
-                return PodDBAdapter.KEY_FILE_URL + " " + "DESC";
+                return PodDBAdapter.KEY_LINK + " " + "DESC";
             case DATE_NEW_OLD:
             default:
                 return PodDBAdapter.TABLE_NAME_FEED_ITEMS + "." + PodDBAdapter.KEY_PUBDATE + " " + "DESC";
