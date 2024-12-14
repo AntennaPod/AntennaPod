61b4ce5dfb60a37c58662cd388e05758e7c7ba2e
Taush Sampley <taushsampley@gmail.com>
2024-12-14 11:38:02 -0600

    Create platform-dependent url encoder

--- net/common/src/main/java/de/danoeh/antennapod/net/common/UriUtil.java
+++ net/common/src/main/java/de/danoeh/antennapod/net/common/UriUtil.java
@@ -1,9 +1,15 @@
 package de.danoeh.antennapod.net.common;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Utility methods for dealing with URL encoding.
@@ -22,5 +28,32 @@ public abstract class UriUtil {
         } catch (MalformedURLException | URISyntaxException e) {
             throw new IllegalArgumentException(e);
         }
+    }
+
+    interface PlatformUrlEncoder {
+        String encode(String input);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    static class StandardUrlEncoder implements PlatformUrlEncoder {
+        @Override
+        public String encode(String input) {
+            return URLEncoder.encode(input, StandardCharsets.UTF_8);
+        }
+    }
+
+    static class LegacyUrlEncoder implements PlatformUrlEncoder {
+        @Override
+        public String encode(String input) {
+            return URLEncoder.encode(input);
+        }
+    }
+
+    private static final PlatformUrlEncoder urlEncoder = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+            ? new StandardUrlEncoder()
+            : new LegacyUrlEncoder();
+
+    public static String urlEncode(String input) {
+        return urlEncoder.encode(input);
     }
 }
