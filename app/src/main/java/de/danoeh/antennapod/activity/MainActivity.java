acac454c922ef8f9e8e13b6898744e54c1b28ae9
ByteHamster <info@bytehamster.com>
2025-02-15 12:02:59 +0100

    Upgrade SearchPreference and use our own 'more' string

--- app/src/main/java/de/danoeh/antennapod/activity/MainActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/MainActivity.java
@@ -535,7 +535,7 @@ public class MainActivity extends CastEnabledActivity {
                     0, getString(NavigationNames.getShortLabel(tag)));
             item.setIcon(NavigationNames.getDrawable(tag));
         }
-        MenuItem moreItem = menu.add(0, R.id.bottom_navigation_more, 0, getString(R.string.searchpreference_more));
+        MenuItem moreItem = menu.add(0, R.id.bottom_navigation_more, 0, getString(R.string.overflow_more));
         moreItem.setIcon(R.drawable.dots_vertical);
         bottomNavigationView.setOnItemSelectedListener(bottomItemSelectedListener);
         updateBottomNavigationBadgeIfNeeded();
