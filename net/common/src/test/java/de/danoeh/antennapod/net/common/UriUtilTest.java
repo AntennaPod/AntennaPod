b1ebef9b8aa7b07a48d57147d93794fda798e833
Taush Sampley <taushsampley@gmail.com>
2025-06-14 13:37:49 -0500

    Convert UriUtil kotlin tests to java

--- net/common/src/test/java/de/danoeh/antennapod/net/common/UriUtilTest.java
+++ net/common/src/test/java/de/danoeh/antennapod/net/common/UriUtilTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Test class for URIUtil
  */
@@ -21,5 +24,21 @@ public class UriUtilTest {
         final String testUrl = "http://example.com/this is not encoded";
         final String expected = "http://example.com/this%20is%20not%20encoded";
         assertEquals(expected, UriUtil.getURIFromRequestUrl(testUrl).toString());
+    }
+
+    @Test
+    public void testUrlEncode() {
+        String testUrl = "http://example.com/this is not encoded";
+        String expected = "http%3A%2F%2Fexample.com%2Fthis+is+not+encoded";
+        assertEquals(expected, UriUtil.urlEncode(testUrl));
+    }
+
+    @Test
+    public void testQueryString() {
+        Map<String, String> params = HashMap.newHashMap(2);
+        params.put("key1", "value1");
+        params.put("key2", "value2");
+        String expected = "key1=value1&key2=value2";
+        assertEquals(expected, UriUtil.queryString(params));
     }
 }
