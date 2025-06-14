8520f409f6c51526421c63a713eb7689d671cf0d
Taush Sampley <taushsampley@gmail.com>
2025-06-14 13:06:51 -0500

    remove import spacing

--- app/src/main/java/de/danoeh/antennapod/ui/share/ShareDialog.java
+++ app/src/main/java/de/danoeh/antennapod/ui/share/ShareDialog.java
@@ -6,12 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-
 import de.danoeh.antennapod.databinding.ShareEpisodeDialogBinding;
 import de.danoeh.antennapod.model.feed.FeedItem;
 
