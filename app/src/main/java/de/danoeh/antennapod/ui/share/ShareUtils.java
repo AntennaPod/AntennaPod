65929728f11d2c8c5c92989fe279dc0e72372725
Taush Sampley <taushsampley@gmail.com>
2025-06-14 13:22:43 -0500

    Move ShareUtils.kt contents to java ShareUtils

--- app/src/main/java/de/danoeh/antennapod/ui/share/ShareUtils.java
+++ app/src/main/java/de/danoeh/antennapod/ui/share/ShareUtils.java
@@ -4,18 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ShareCompat;
 import androidx.core.content.FileProvider;
-
 import java.io.File;
-
+import java.util.Map;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.model.feed.Feed;
 import de.danoeh.antennapod.model.feed.FeedItem;
 import de.danoeh.antennapod.model.feed.FeedMedia;
-import de.danoeh.antennapod.net.common.UriUtilKt;
+import de.danoeh.antennapod.net.common.UriUtil;
 import de.danoeh.antennapod.ui.common.Converter;
 
 /** Utility methods for sharing data */
@@ -35,12 +33,28 @@ public class ShareUtils {
         context.startActivity(intent);
     }
 
+    /**
+     * Builds a shareable link for this feed item, including media URL, title, GUID, and publication date.
+     */
+    public static String getShareLink(@NonNull FeedItem item) {
+        String query = UriUtil.queryString(
+                Map.of(
+                        "url", item.getMedia() != null ? item.getMedia().getDownloadUrl() : "",
+                        "eTitle", item.getTitle(),
+                        "eGuid", Long.toString(item.getId()),
+                        "eDate", String.valueOf(item.getPubDate().getTime())
+                )
+        );
+        return String.format("https://antennapod.org/deeplink/episode/?%s", query);
+    }
+
+
     public static void shareFeedLink(@NonNull Context context, @NonNull Feed feed) {
-        String feedurl = UriUtilKt.urlEncode(feed.getDownloadUrl());
+        String feedurl = UriUtil.urlEncode(feed.getDownloadUrl());
         feedurl = feedurl.replace("htt", "%68%74%74"); // To not confuse users by having a url inside a url
         String text = feed.getTitle() + "\n\n"
                 + "https://antennapod.org/deeplink/subscribe/?url=" + feedurl
-                + "&title=" + UriUtilKt.urlEncode(feed.getTitle());
+                + "&title=" + UriUtil.urlEncode(feed.getTitle());
         shareLink(context, text);
     }
 
@@ -80,7 +94,7 @@ public class ShareUtils {
                 text += "\n";
             }
             text += "\n" + context.getResources().getString(R.string.share_dialog_media_file_label) + ": ";
-            String shareText = ShareUtilsKt.getShareLink(item);
+            String shareText = ShareUtils.getShareLink(item);
             if (abbreviate && shareText.length() > ABBREVIATE_MAX_LENGTH) {
                 text += shareText.substring(0, ABBREVIATE_MAX_LENGTH) + "…";
             } else {
