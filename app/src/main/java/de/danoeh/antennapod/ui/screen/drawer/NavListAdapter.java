e17f6b7d87d0dd27cb83987596108cd023ad58f7
Soumyadip Das <dassoumyadip7407@gmail.com>
2025-07-28 01:56:30 +0530

    chore: Apply code formatting to NavListAdapter file

--- app/src/main/java/de/danoeh/antennapod/ui/screen/drawer/NavListAdapter.java
+++ app/src/main/java/de/danoeh/antennapod/ui/screen/drawer/NavListAdapter.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
