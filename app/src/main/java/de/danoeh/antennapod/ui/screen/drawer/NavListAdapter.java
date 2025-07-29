70e1153d96797a79288da346246923baa476d54d
Soumyadip Das <dassoumyadip7407@gmail.com>
2025-07-29 11:22:47 +0530

    Revert "Revert commit from issue 7571"
    This reverts commit e17f6b7d87d0dd27cb83987596108cd023ad58f7.

--- app/src/main/java/de/danoeh/antennapod/ui/screen/drawer/NavListAdapter.java
+++ app/src/main/java/de/danoeh/antennapod/ui/screen/drawer/NavListAdapter.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
