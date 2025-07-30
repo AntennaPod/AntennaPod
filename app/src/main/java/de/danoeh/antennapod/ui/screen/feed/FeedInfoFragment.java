71233c05c2f82f3d61b9b320daa30c5c1c2d6fee
Matej Drobnič <matej@matejdro.com>
2025-07-30 09:33:27 +0200

    Fix local feed delete not working after a while

--- app/src/main/java/de/danoeh/antennapod/ui/screen/feed/FeedInfoFragment.java
+++ app/src/main/java/de/danoeh/antennapod/ui/screen/feed/FeedInfoFragment.java
@@ -344,7 +344,8 @@ public class FeedInfoFragment extends Fragment implements MaterialToolbar.OnMenu
 
         Completable.fromAction(() -> {
             getActivity().getContentResolver()
-                    .takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    .takePersistableUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION
+                            | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             DocumentFile documentFile = DocumentFile.fromTreeUri(getContext(), uri);
             if (documentFile == null) {
                 throw new IllegalArgumentException("Unable to retrieve document tree");
@@ -364,7 +365,10 @@ public class FeedInfoFragment extends Fragment implements MaterialToolbar.OnMenu
         @Override
         public Intent createIntent(@NonNull final Context context, @Nullable final Uri input) {
             return super.createIntent(context, input)
-                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                            | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                            | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+                    );
         }
     }
 }
