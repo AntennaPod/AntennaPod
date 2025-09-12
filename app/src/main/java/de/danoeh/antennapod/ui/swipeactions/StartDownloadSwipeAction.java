41134250d3f338492445d2b138cd9defa7bb6101
ByteHamster <info@bytehamster.com>
2025-09-12 21:50:44 +0200

    Tweak error messages when interacting with text only episode

--- app/src/main/java/de/danoeh/antennapod/ui/swipeactions/StartDownloadSwipeAction.java
+++ app/src/main/java/de/danoeh/antennapod/ui/swipeactions/StartDownloadSwipeAction.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import androidx.fragment.app.Fragment;
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.actionbutton.DownloadActionButton;
+import de.danoeh.antennapod.event.MessageEvent;
 import de.danoeh.antennapod.model.feed.FeedItem;
 import de.danoeh.antennapod.model.feed.FeedItemFilter;
+import org.greenrobot.eventbus.EventBus;
 
 public class StartDownloadSwipeAction implements SwipeAction {
 
@@ -31,9 +33,12 @@ public class StartDownloadSwipeAction implements SwipeAction {
 
     @Override
     public void performAction(FeedItem item, Fragment fragment, FeedItemFilter filter) {
-        if (!item.isDownloaded() && !item.getFeed().isLocalFeed()) {
-            new DownloadActionButton(item)
-                    .onClick(fragment.requireContext());
+        if (item.getMedia() == null) {
+            EventBus.getDefault().post(new MessageEvent(fragment.getString(R.string.no_media_label)));
+        } else if (item.getFeed().isLocalFeed() || item.isDownloaded()) {
+            EventBus.getDefault().post(new MessageEvent(fragment.getString(R.string.already_downloaded)));
+        } else {
+            new DownloadActionButton(item).onClick(fragment.requireContext());
         }
     }
 
