7751704aa5be17096cff58f105e9e6acb4312f0a
Hans-Peter Lehmann <ByteHamster@users.noreply.github.com>
2026-02-22 21:56:09 +0100

    Small ExoPlayer tweaks (#8305)
    ### Description
    
    Small ExoPlayer tweaks:
    - Cache backwards and forwards
    - Fix getting stuck when dragging playback speed bar
    - Exact seeking
    - Code cleanup
    - +/- speed buttons increment by 0.05 like the seekbar
    
    ### Checklist
    <!-- 
    To help us keep the issue tracker clean and work as efficient as
    possible,
      please make sure that you have done all of the following.
    You can tick the boxes below by placing an x inside the brackets like
    this: [x]
    -->
    - [x] I have read the contribution guidelines:
    https://github.com/AntennaPod/AntennaPod/blob/develop/CONTRIBUTING.md#submit-a-pull-request
    - [x] I have performed a self-review of my code, going through my
    changes line by line and carefully considering why this line change is
    necessary
    - [x] I have run the automated code checks using `./gradlew checkstyle
    spotbugsPlayDebug spotbugsDebug :app:lintPlayDebug`
    - [x] My code follows the style guidelines of the AntennaPod project:
    https://antennapod.org/contribute/develop/app/code-style
    - [x] I have mentioned the corresponding issue and the relevant keyword
    (e.g., "Closes: #xy") in the description (see
    https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    - [x] If it is a core feature, I have added automated tests

---
+++ playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/ExoPlayerUtils.java
@@ -0,0 +1,61 @@
+package de.danoeh.antennapod.playback.service.internal;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
+import androidx.media3.common.AudioAttributes;
+import androidx.media3.common.C;
+import androidx.media3.common.PlaybackException;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.datasource.HttpDataSource;
+import androidx.media3.exoplayer.DefaultLoadControl;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.SeekParameters;
+import de.danoeh.antennapod.net.common.NetworkUtils;
+import de.danoeh.antennapod.playback.service.R;
+import de.danoeh.antennapod.storage.preferences.UserPreferences;
+
+public class ExoPlayerUtils {
+    @OptIn(markerClass = UnstableApi.class)
+    public static ExoPlayer buildPlayer(Context context) {
+        return new ExoPlayer.Builder(context)
+                .setSeekBackIncrementMs(UserPreferences.getRewindSecs() * 1000L)
+                .setSeekForwardIncrementMs(UserPreferences.getFastForwardSecs() * 1000L)
+                .setLoadControl(new DefaultLoadControl.Builder()
+                        .setBufferDurationsMs((int) (UserPreferences.getFastForwardSecs() * 1000L),
+                                DefaultLoadControl.DEFAULT_MAX_BUFFER_MS,
+                                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+                                DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS)
+                        .setBackBuffer((int) (3 * UserPreferences.getRewindSecs() * 1000L), true)
+                        .build())
+                .setAudioAttributes(new AudioAttributes.Builder()
+                        .setUsage(C.USAGE_MEDIA)
+                        .setContentType(C.AUDIO_CONTENT_TYPE_SPEECH)
+                        .build(), true)
+                .setSeekParameters(SeekParameters.EXACT)
+                .build();
+    }
+
+    public static String translateErrorReason(@NonNull PlaybackException error, Context context) {
+        if (NetworkUtils.wasDownloadBlocked(error)) {
+            return context.getString(R.string.download_error_blocked);
+        }
+
+        Throwable cause = error.getCause();
+        if (cause instanceof HttpDataSource.HttpDataSourceException) {
+            if (cause.getCause() != null) {
+                cause = cause.getCause();
+            }
+        }
+        if (cause != null && "Source error".equals(cause.getMessage())) {
+            cause = cause.getCause();
+        }
+        if (cause != null && cause.getMessage() != null) {
+            return cause.getMessage();
+        } else if (error.getMessage() != null && cause != null) {
+            return error.getMessage() + ": " + cause.getClass().getSimpleName();
+        } else {
+            return "Unknown error";
+        }
+    }
+}
