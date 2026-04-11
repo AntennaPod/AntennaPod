33066183e7b55a6917b9013fe44414f4860f5728
Michael Pollmeier <michael@michaelpollmeier.com>
2026-04-11 16:51:15 +0200

    temp: println debuggin to find a way to identify a child device
    adb logcat -s ParentalControl:
    unrestricted device:
    ```
    04-11 16:49:31.256 31740 31740 D ParentalControl: All accounts (0):
    04-11 16:49:31.257 31740 31740 D ParentalControl: Google accounts: 0
    04-11 16:49:31.257 31740 31740 D ParentalControl: UserRestrictions:
    04-11 16:49:31.257 31740 31740 D ParentalControl:   no_record_audio=false
    04-11 16:49:31.257 31740 31740 D ParentalControl: DISALLOW_INSTALL_APPS=false DISALLOW_UNINSTALL_APPS=false
    04-11 16:49:31.257 31740 31740 D ParentalControl: package com.google.android.apps.kids.familylinkhelper installed=false
    04-11 16:49:31.257 31740 31740 D ParentalControl: package com.google.android.apps.kids.familylink installed=false
    04-11 16:49:31.258 31740 31740 D ParentalControl: package com.google.android.apps.kids.home installed=false
    ```
    
    child device:
    ```
    04-11 16:42:15.912 26356 26356 D ParentalControl: All accounts (0):
    04-11 16:42:15.913 26356 26356 D ParentalControl: Google accounts: 0
    04-11 16:42:15.914 26356 26356 D ParentalControl: UserRestrictions:
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_add_private_profile=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_record_audio=false
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_add_user=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   disallow_config_default_apps=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_add_managed_profile=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_install_unknown_sources=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_factory_reset=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_config_location=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_add_clone_profile=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_safe_boot=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_remove_user=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_config_credentials=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_grant_admin=true
    04-11 16:42:15.914 26356 26356 D ParentalControl:   no_config_date_time=true
    04-11 16:42:15.915 26356 26356 D ParentalControl: DISALLOW_INSTALL_APPS=false DISALLOW_UNINSTALL_APPS=false
    04-11 16:42:15.915 26356 26356 D ParentalControl: package com.google.android.apps.kids.familylinkhelper installed=false
    04-11 16:42:15.916 26356 26356 D ParentalControl: package com.google.android.apps.kids.familylink installed=false
    04-11 16:42:15.916 26356 26356 D ParentalControl: package com.google.android.apps.kids.home installed=false
    ```

--- app/src/main/java/de/danoeh/antennapod/ui/screen/preferences/MainPreferencesFragment.java
+++ app/src/main/java/de/danoeh/antennapod/ui/screen/preferences/MainPreferencesFragment.java
@@ -146,22 +146,62 @@ public class MainPreferencesFragment extends AnimatedPreferenceFragment {
 
     // show the 'parental password' preference if we're on a 'child' device (family link) or if it's a debug build
     private void setParentalControlsVisibility() {
-        if (BuildConfig.DEBUG) {
-            findPreference(PREF_PARENTAL_CONTROL_PASSWORD).setVisible(true);
-        } else {
-            findPreference(PREF_PARENTAL_CONTROL_PASSWORD).setVisible(false);
-            AccountManager am = AccountManager.get(requireContext());
-            for (Account account : am.getAccountsByType("com.google")) {
-                am.hasFeatures(account, new String[]{"child"}, future -> {
+        findPreference(PREF_PARENTAL_CONTROL_PASSWORD).setVisible(false);
+
+        // --- Approach 1: AccountManager - all accounts ---
+        AccountManager am = AccountManager.get(requireContext());
+        Account[] allAccounts = am.getAccounts();
+        android.util.Log.d("ParentalControl", "All accounts (" + allAccounts.length + "):");
+        for (Account account : allAccounts) {
+            android.util.Log.d("ParentalControl", "  name=" + account.name + " type=" + account.type);
+        }
+
+        // --- Approach 2: AccountManager feature checks on all Google accounts ---
+        String[] featuresToCheck = {"child", "service_uca_familylink", "features_FAMILY_LINK_CHILD"};
+        Account[] googleAccounts = am.getAccountsByType("com.google");
+        android.util.Log.d("ParentalControl", "Google accounts: " + googleAccounts.length);
+        for (Account account : googleAccounts) {
+            for (String feature : featuresToCheck) {
+                am.hasFeatures(account, new String[]{feature}, future -> {
                     try {
-                        if (future.getResult()) {
+                        boolean has = future.getResult();
+                        android.util.Log.d("ParentalControl", "  " + account.name + " feature=" + feature + " -> " + has);
+                        if (has) {
                             findPreference(PREF_PARENTAL_CONTROL_PASSWORD).setVisible(true);
                         }
                     } catch (AuthenticatorException | OperationCanceledException | IOException e) {
-                        e.printStackTrace();
+                        android.util.Log.e("ParentalControl", "  error for feature=" + feature, e);
                     }
                 }, new Handler(Looper.getMainLooper()));
             }
+        }
+
+        // --- Approach 3: UserManager restrictions set by Family Link ---
+        android.os.UserManager um = requireContext().getSystemService(android.os.UserManager.class);
+        android.os.Bundle restrictions = um.getUserRestrictions();
+        android.util.Log.d("ParentalControl", "UserRestrictions:");
+        for (String key : restrictions.keySet()) {
+            android.util.Log.d("ParentalControl", "  " + key + "=" + restrictions.get(key));
+        }
+        boolean disallowInstall = um.hasUserRestriction(android.os.UserManager.DISALLOW_INSTALL_APPS);
+        boolean disallowUninstall = um.hasUserRestriction(android.os.UserManager.DISALLOW_UNINSTALL_APPS);
+        android.util.Log.d("ParentalControl", "DISALLOW_INSTALL_APPS=" + disallowInstall
+                + " DISALLOW_UNINSTALL_APPS=" + disallowUninstall);
+
+        // --- Approach 4: Check if Family Link helper app is installed ---
+        String[] familyLinkPackages = {
+                "com.google.android.apps.kids.familylinkhelper",
+                "com.google.android.apps.kids.familylink",
+                "com.google.android.apps.kids.home"
+        };
+        android.content.pm.PackageManager pm = requireContext().getPackageManager();
+        for (String pkg : familyLinkPackages) {
+            boolean installed = false;
+            try {
+                pm.getPackageInfo(pkg, 0);
+                installed = true;
+            } catch (android.content.pm.PackageManager.NameNotFoundException ignored) { }
+            android.util.Log.d("ParentalControl", "package " + pkg + " installed=" + installed);
         }
     }
 
