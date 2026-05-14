9e18b674b21d1573971a5eb8aed8a21e4b3d6f0d
Hans-Peter Lehmann <ByteHamster@users.noreply.github.com>
2026-05-14 12:19:23 +0200

    Support password protected media streams in new playback service (#8430)
    ### Description
    
    Support password protected media streams in new playback service
    
    ### Checklist
    <!-- 
    To help us keep the issue tracker clean and work as efficient as
    possible,
      please make sure that you have done all of the following.
    You can tick the boxes below by placing an x inside the brackets like
    this: [x]
    -->
    - [x] I have read the contribution guidelines:
    https://github.com/AntennaPod/AntennaPod/blob/develop/CONTRIBUTING.md#submit-a-pull-request
    - [x] I have performed a self-review of my code, going through my
    changes line by line and carefully considering why this line change is
    necessary
    - [x] I have run the automated code checks using `./gradlew checkstyle
    lint spotbugsPlayDebug spotbugsDebug`
    - [x] My code follows the style guidelines of the AntennaPod project:
    https://antennapod.org/contribute/develop/app/code-style
    - [x] I have mentioned the corresponding issue and the relevant keyword
    (e.g., "Closes: #xy") in the description (see
    https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    - [x] If it is a core feature, I have added automated tests

--- playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/ExoPlayerUtils.java
+++ playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/ExoPlayerUtils.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.OptIn;
 import androidx.media3.common.AudioAttributes;
 import androidx.media3.common.C;
+import androidx.media3.common.MediaItem;
 import androidx.media3.common.PlaybackException;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.datasource.DefaultDataSource;
@@ -13,30 +14,23 @@ import androidx.media3.datasource.HttpDataSource;
 import androidx.media3.exoplayer.DefaultLoadControl;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.exoplayer.SeekParameters;
-import androidx.media3.exoplayer.source.ProgressiveMediaSource;
+import androidx.media3.exoplayer.drm.DrmSessionManagerProvider;
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
+import androidx.media3.exoplayer.source.MediaSource;
+import androidx.media3.exoplayer.upstream.LoadErrorHandlingPolicy;
 import androidx.media3.extractor.DefaultExtractorsFactory;
 import androidx.media3.extractor.mp3.Mp3Extractor;
 import de.danoeh.antennapod.net.common.NetworkUtils;
 import de.danoeh.antennapod.net.common.UserAgentInterceptor;
+import de.danoeh.antennapod.playback.base.MediaItemAdapter;
 import de.danoeh.antennapod.playback.service.R;
 import de.danoeh.antennapod.storage.preferences.UserPreferences;
+
+import java.util.Collections;
 
 public class ExoPlayerUtils {
     @OptIn(markerClass = UnstableApi.class)
     public static ExoPlayer buildPlayer(Context context) {
-
-        final DefaultHttpDataSource.Factory httpDataSourceFactory = new DefaultHttpDataSource.Factory();
-        httpDataSourceFactory.setUserAgent(UserAgentInterceptor.USER_AGENT);
-        httpDataSourceFactory.setAllowCrossProtocolRedirects(true);
-        httpDataSourceFactory.setKeepPostFor302Redirects(true);
-
-        final DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-        extractorsFactory.setConstantBitrateSeekingEnabled(true);
-        extractorsFactory.setMp3ExtractorFlags(Mp3Extractor.FLAG_DISABLE_ID3_METADATA);
-
-        ProgressiveMediaSource.Factory mediaSourceFactory = new ProgressiveMediaSource.Factory(
-                new DefaultDataSource.Factory(context, httpDataSourceFactory), extractorsFactory);
-
         return new ExoPlayer.Builder(context)
                 .setLoadControl(new DefaultLoadControl.Builder()
                         .setBufferDurationsMs(
@@ -51,7 +45,7 @@ public class ExoPlayerUtils {
                         .setUsage(C.USAGE_MEDIA)
                         .setContentType(C.AUDIO_CONTENT_TYPE_SPEECH)
                         .build(), true)
-                .setMediaSourceFactory(mediaSourceFactory)
+                .setMediaSourceFactory(new ApMediaSourceFactory(context))
                 .setSeekParameters(SeekParameters.EXACT)
                 .build();
     }
@@ -76,6 +70,71 @@ public class ExoPlayerUtils {
             return error.getMessage() + ": " + cause.getClass().getSimpleName();
         } else {
             return "Unknown error";
+        }
+    }
+
+    @OptIn(markerClass = UnstableApi.class)
+    public static class ApMediaSourceFactory implements MediaSource.Factory {
+        private LoadErrorHandlingPolicy loadErrorHandlingPolicy;
+        private DrmSessionManagerProvider drmSessionManagerProvider;
+        private final DefaultExtractorsFactory extractorsFactory;
+        private final DefaultMediaSourceFactory defaultFactory;
+        private final Context context;
+
+        public ApMediaSourceFactory(Context context) {
+            super();
+            this.context = context;
+            this.extractorsFactory = new DefaultExtractorsFactory();
+            this.extractorsFactory.setConstantBitrateSeekingEnabled(true);
+            this.extractorsFactory.setMp3ExtractorFlags(Mp3Extractor.FLAG_DISABLE_ID3_METADATA);
+            this.defaultFactory = new DefaultMediaSourceFactory(context, extractorsFactory);
+        }
+
+        @Override
+        public MediaSource.Factory setDrmSessionManagerProvider(DrmSessionManagerProvider drmSessionManagerProvider) {
+            this.drmSessionManagerProvider = drmSessionManagerProvider;
+            return this;
+        }
+
+        @Override
+        public MediaSource.Factory setLoadErrorHandlingPolicy(LoadErrorHandlingPolicy policy) {
+            this.loadErrorHandlingPolicy = policy;
+            return this;
+        }
+
+        @Override
+        public MediaSource createMediaSource(MediaItem mediaItem) {
+            DefaultMediaSourceFactory factory = new DefaultMediaSourceFactory(
+                    buildDataSourceFactory(mediaItem), extractorsFactory);
+            if (loadErrorHandlingPolicy != null) {
+                factory.setLoadErrorHandlingPolicy(loadErrorHandlingPolicy);
+            }
+            if (drmSessionManagerProvider != null) {
+                factory.setDrmSessionManagerProvider(drmSessionManagerProvider);
+            }
+            return factory.createMediaSource(mediaItem);
+        }
+
+        private DefaultDataSource.Factory buildDataSourceFactory(MediaItem mediaItem) {
+            DefaultHttpDataSource.Factory httpDataSourceFactory =
+                    new DefaultHttpDataSource.Factory();
+            httpDataSourceFactory.setUserAgent(UserAgentInterceptor.USER_AGENT);
+            httpDataSourceFactory.setAllowCrossProtocolRedirects(true);
+            httpDataSourceFactory.setKeepPostFor302Redirects(true);
+            String authHeader = mediaItem.requestMetadata.extras != null
+                    ? mediaItem.requestMetadata.extras.getString(
+                            MediaItemAdapter.KEY_AUTHORIZATION_HEADER)
+                    : null;
+            if (authHeader != null) {
+                httpDataSourceFactory.setDefaultRequestProperties(
+                        Collections.singletonMap("Authorization", authHeader));
+            }
+            return new DefaultDataSource.Factory(context, httpDataSourceFactory);
+        }
+
+        @Override
+        public int[] getSupportedTypes() {
+            return defaultFactory.getSupportedTypes();
         }
     }
 }
