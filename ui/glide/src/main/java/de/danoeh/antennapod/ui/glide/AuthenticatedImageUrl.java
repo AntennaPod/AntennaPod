4b606e6d64908b08a3b2a671f899a4e03282850f
Tony Tam <engineer.tonytam@gmail.com>
2026-08-18 21:55:02 -0700

    Fix annotation, add equals/hashCode, thread auth in ChaptersListAdapter
    - Change @NonNull to @Nullable on AuthenticatedImageUrl.create() since
      it can return null when the url parameter is null
    - Add equals()/hashCode() so Glide can deduplicate in-flight requests
      for the same URL+credentials during RecyclerView rebinds
    - Thread feed credentials through ChaptersListAdapter fallback image
      load so chapter images also get auth headers

--- ui/glide/src/main/java/de/danoeh/antennapod/ui/glide/AuthenticatedImageUrl.java
+++ ui/glide/src/main/java/de/danoeh/antennapod/ui/glide/AuthenticatedImageUrl.java
@@ -3,10 +3,11 @@ package de.danoeh.antennapod.ui.glide;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import de.danoeh.antennapod.model.feed.FeedPreferences;
+import java.util.Objects;
 
 public class AuthenticatedImageUrl {
 
-    @NonNull
+    @Nullable
     public static Object create(@Nullable String url, @Nullable FeedPreferences prefs) {
         if (url != null && prefs != null && prefs.getUsername() != null && !prefs.getUsername().isEmpty()) {
             return new AuthenticatedImageUrl(url, prefs.getUsername(), prefs.getPassword());
@@ -44,5 +45,24 @@ public class AuthenticatedImageUrl {
 
     public boolean hasCredentials() {
         return username != null && !username.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof AuthenticatedImageUrl)) {
+            return false;
+        }
+        AuthenticatedImageUrl that = (AuthenticatedImageUrl) o;
+        return url.equals(that.url)
+                && Objects.equals(username, that.username)
+                && Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, username, password);
     }
 }
