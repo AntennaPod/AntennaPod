535fedb0183153a6554ae9f67a7aa213cbec056a
Hans-Peter Lehmann <ByteHamster@users.noreply.github.com>
2026-08-23 19:15:14 +0200

    Add more logs to playback resumption (#8686)
    ### Description
    
    Add more logs to playback resumption. This way, we can maybe figure out
    #8666 and #8650 more easily
    
    ### Checklist
    <!-- 
    To help us keep the issue tracker clean and work as efficient as
    possible,
      please make sure that you have done all of the following.
    You can tick the boxes below by placing an x inside the brackets like
    this: [x]
    -->
    - [x] I have read the contribution guidelines:
    https://github.com/AntennaPod/AntennaPod/blob/develop/CONTRIBUTING.md#submit-a-pull-request
    - [x] I have performed a self-review of my code, going through my
    changes line by line and carefully considering why this line change is
    necessary
    - [x] I have run the automated code checks using `./gradlew checkstyle
    lint`
    - [x] My code follows the style guidelines of the AntennaPod project:
    https://antennapod.org/contribute/develop/app/code-style
    - [x] I have mentioned the corresponding issue and the relevant keyword
    (e.g., "Closes: #xy") in the description (see
    https://docs.github.com/en/issues/tracking-your-work-with-issues/linking-a-pull-request-to-an-issue)
    - [x] If it is a core feature, I have added automated tests

--- playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/MediaLibrarySessionCallback.java
+++ playback/service/src/main/java/de/danoeh/antennapod/playback/service/internal/MediaLibrarySessionCallback.java
@@ -358,17 +358,20 @@ public class MediaLibrarySessionCallback implements MediaLibraryService.MediaLib
     @NonNull
     public ListenableFuture<MediaSession.MediaItemsWithStartPosition> onPlaybackResumption(
             @NonNull MediaSession mediaSession, @NonNull MediaSession.ControllerInfo controller) {
+        Log.d(TAG, "onPlaybackResumption() called");
         SettableFuture<MediaSession.MediaItemsWithStartPosition> future = SettableFuture.create();
         Single.fromCallable(() -> {
             FeedMedia media = DBReader.getFeedMedia(PlaybackPreferences.getCurrentlyPlayingFeedMediaId());
             // If there is no media to resume, media3 crashes. So instead of crashing, just play something random.
             if (media == null) {
+                Log.d(TAG, "onPlaybackResumption: trying paused queue now");
                 List<FeedItem> recentQueue = DBReader.getPausedQueue(1);
                 if (!recentQueue.isEmpty()) {
                     media = recentQueue.get(0).getMedia();
                 }
             }
             if (media == null) {
+                Log.d(TAG, "onPlaybackResumption: trying recent episodes now");
                 List<FeedItem> items = DBReader.getEpisodes(0, 1, FeedItemFilter.unfiltered(), SortOrder.DATE_NEW_OLD);
                 if (!items.isEmpty()) {
                     media = items.get(0).getMedia();
@@ -382,6 +385,9 @@ public class MediaLibrarySessionCallback implements MediaLibraryService.MediaLib
                             long startPosition = SkipUtils.skipIntroIfNecessary(context, media);
                             startPosition = RewindAfterPauseUtils.calculatePositionWithRewind(
                                     (int) startPosition, media.getLastPlayedTimeStatistics());
+                            Log.d(TAG, "onPlaybackResumption: resuming id=" + media.getId()
+                                    + ", title=" + (media.getItem() != null ? media.getItem().getTitle() : "null")
+                                    + ", startPosition=" + startPosition);
                             MediaSession.MediaItemsWithStartPosition result =
                                     new MediaSession.MediaItemsWithStartPosition(
                                             Collections.singletonList(
